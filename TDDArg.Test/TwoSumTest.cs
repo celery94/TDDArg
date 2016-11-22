013e07c1549ff82bd47b699947599c2985562699
Celery Liu <celery.liu@commscope.com>
2016-11-22 12:56:40 +0800

    Two Sum

---
+++ TDDArg.Test/TwoSumTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class TwoSumTest
+    {
+        [Test]
+        public void TwoSum_One()
+        {
+            int[] nums = new int[] { 2, 7 };
+            Assert.AreEqual(new int[] { 0, 1 }, TwoSum.TwoSumFunc(nums, 9));
+        }
+
+        [Test]
+        public void TwoSum_Two()
+        {
+            int[] nums = new int[] { 2, 7, 1 };
+            Assert.AreEqual(new int[] { 0, 2 }, TwoSum.TwoSumFunc(nums, 3));
+        }
+    }
+}
