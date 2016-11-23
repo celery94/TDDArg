4ffa82a274888c8aa5c6d54964d39c90e60ad34a
Celery Liu <celery.liu@commscope.com>
2016-11-23 16:31:24 +0800

    Three sum

---
+++ TDDArg.Test/ThreeSumTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class ThreeSumTest
+    {
+
+        [Test]
+        public void ThreeSum_Test()
+        {
+            int[] nums = new[] { -1, 0, 1, 2, -1, -4 };
+
+            var result = new List<List<int>>()
+            {
+                new List<int>() { -1, 0, 1 },
+                new List<int>() { -1, -1, 2 }
+            };
+
+            Assert.AreEqual(result,ThreeSum.ThreeSumFunc(nums));
+        }
+    }
+}
---
+++ TDDArg/ThreeSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDDArg
+{
+    public class ThreeSum
+    {
+        public static List<List<int>> ThreeSumFunc(int[] nums)
+        {
+            List<List<int>> result = new List<List<int>>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    for (int k = j + 1; k < nums.Length; k++)
+                    {
+                        if (nums[i] + nums[j] + nums[k] == 0)
+                        {
+                            result.Add(new List<int>() { nums[i] , nums[j] , nums[k] });
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
