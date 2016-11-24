898a0f0b29255d28bdf6fd1c6c9e006682ed5c52
Celery Liu <celery.liu@commscope.com>
2016-11-24 17:14:50 +0800

    LongestPalindrome With Issue

---
+++ TDDArg.Test/LongestPalindromeFuncTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class LongestPalindromeFuncTest
+    {
+        [Test]
+        public void LongestPalindrome_Even_Result()
+        {
+            var input = "babad";
+
+            var ret = LongestPalindrome.LongestPalindromeFunc(input);
+
+            Assert.AreEqual("bab", ret);
+        }
+
+        [Test]
+        public void LongestPalindrome_Even_Result_Single()
+        {
+            var input = "a";
+
+            var ret = LongestPalindrome.LongestPalindromeFunc(input);
+
+            Assert.AreEqual("a", ret);
+        }
+
+        [Test]
+        public void LongestPalindrome_Even_Result_M()
+        {
+            var input = "aaa";
+
+            var ret = LongestPalindrome.LongestPalindromeFunc(input);
+
+            Assert.AreEqual("aaa", ret);
+        }
+
+        [Test]
+        public void LongestPalindrome_Odd_Result_Single()
+        {
+            var input = "aa";
+
+            var ret = LongestPalindrome.LongestPalindromeFunc(input);
+
+            Assert.AreEqual("aa", ret);
+        }
+
+        [Test]
+        public void LongestPalindrome_Odd_Result()
+        {
+            var input = "cbbd";
+
+            var ret = LongestPalindrome.LongestPalindromeFunc(input);
+
+            Assert.AreEqual("bb", ret);
+        }
+    }
+}
---
+++ TDDArg/LongestPalindrome.cs
@@ -0,0 +1,38 @@
+﻿namespace TDDArg
+{
+    public class LongestPalindrome
+    {
+        public static string LongestPalindromeFunc(string s)
+        {
+            var result = string.Empty;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var left = i;
+                var right = i;
+                if (right + 1 < s.Length && s[left] == s[right + 1])
+                {
+                    right++;
+                }
+                while (left >= 0 && right < s.Length)
+                {
+                    if (s[left] == s[right])
+                    {
+                        if (right - left + 1 > result.Length)
+                        {
+                            result = s.Substring(left, right - left + 1);
+                        }
+
+                        left--;
+                        right++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
