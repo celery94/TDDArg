0b74f022a0407ecad34310a23b76fcd68c33f0e7
Celery Liu <celery.liu@commscope.com>
2016-11-24 16:11:44 +0800

    FindMedianSortedArrays

---
+++ TDDArg.Test/FindMedianSortedArraysTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class FindMedianSortedArraysTest
+    {
+        [Test]
+        public void FindMedianSortedArrays_Even_Test()
+        {
+            var nums1 = new[] {1, 3};
+            var nums2 = new[] {2};
+
+            double ret = FindMedianSortedArrays.FindMedianSortedArraysFunc(nums1, nums2);
+
+            Assert.AreEqual((double) 2, ret);
+        }
+
+        [Test]
+        public void FindMedianSortedArrays_Odd_Test()
+        {
+            var nums1 = new[] {1, 3};
+            var nums2 = new[] {2, 4};
+
+            double ret = FindMedianSortedArrays.FindMedianSortedArraysFunc(nums1, nums2);
+
+            Assert.AreEqual((double) 2.5, ret);
+        }
+
+        [Test]
+        public void FindMedianSortedArrays_Odd_Sort_Test()
+        {
+            var nums1 = new[] {1, 2};
+            var nums2 = new[] {3, 4};
+
+            double ret = FindMedianSortedArrays.FindMedianSortedArraysFunc(nums1, nums2);
+
+            Assert.AreEqual((double) 2.5, ret);
+        }
+    }
+}
---
+++ TDDArg/FindMedianSortedArrays.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TDDArg
+{
+    public class FindMedianSortedArrays
+    {
+        public static double FindMedianSortedArraysFunc(int[] nums1, int[] nums2)
+        {
+            var middleIndex = (nums1.Length + nums2.Length) / 2;
+
+            int startIndex = 0, index1 = 0, index2 = 0;
+            int result = 0, preresult = 0;
+            while (startIndex <= middleIndex)
+            {
+                if (index2 > nums2.Length - 1 || (index1 < nums1.Length && nums1[index1] <= nums2[index2]))
+                {
+                    preresult = result;
+                    result = nums1[index1];
+                    index1++;
+                }
+                else
+                {
+                    preresult = result;
+                    result = nums2[index2];
+                    index2++;
+                }
+
+                startIndex++;
+            }
+            if ((nums1.Length + nums2.Length) % 2 == 1)
+                return (double) result;
+            else
+                return (double) (result + preresult) / 2;
+        }
+    }
+}
