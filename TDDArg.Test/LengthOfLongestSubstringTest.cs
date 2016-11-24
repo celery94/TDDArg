665da0be9036bf7513421cde8bfb83f8d3479324
Celery Liu <celery.liu@commscope.com>
2016-11-24 14:14:58 +0800

    LengthOfLongestSubstring

---
+++ TDDArg.Test/LengthOfLongestSubstringTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class LengthOfLongestSubstringTest
+    {
+        [Test]
+        public void LengthOfLongestSubstring_One()
+        {
+            Assert.AreEqual(0,LengthOfLongestSubstring.LengthOfLongestSubstringFunc(""));
+            Assert.AreEqual(1,LengthOfLongestSubstring.LengthOfLongestSubstringFunc("bbb"));
+            Assert.AreEqual(2,LengthOfLongestSubstring.LengthOfLongestSubstringFunc("au"));
+            Assert.AreEqual(3,LengthOfLongestSubstring.LengthOfLongestSubstringFunc("abcabcbb"));
+            Assert.AreEqual(3,LengthOfLongestSubstring.LengthOfLongestSubstringFunc("pwwkew"));
+        }
+    }
+}
---
+++ TDDArg/LengthOfLongestSubstring.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TDDArg
+{
+    public class LengthOfLongestSubstring
+    {
+        public static int LengthOfLongestSubstringFunc(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s)) return 0;
+
+            int start = 0;
+            int end = 0;
+
+            int count = end - start + 1;
+            while (end < s.Length)
+            {
+                if (start == end)
+                {
+                    if (end - start + 1 > count)
+                    {
+                        count = end - start + 1;
+                    }
+
+                    end++;
+                }
+                else
+                {
+                    var index = s.Substring(start, end - start).IndexOf(s[end]);
+
+                    if (index != -1)
+                    {
+                        start = start + index + 1;
+                        end++;
+                    }
+                    else
+                    {
+                        if (end - start + 1 > count)
+                        {
+                            count = end - start + 1;
+                        }
+
+                        end++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
