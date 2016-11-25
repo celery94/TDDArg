0ecc72634e75c6ef12f4a82eea0b149698e2c156
Celery Liu <celery.liu@commscope.com>
2016-11-25 12:58:57 +0800

    ZigZagConversion wrong answer

---
+++ TDDArg.Test/ZigZagConversionTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class ZigZagConversionTest
+    {
+        [Test]
+        public void Convert_1_Row()
+        {
+            var input = "PAYPALISHIRING";
+
+            var result = ZigZagConversion.Convert(input, 1);
+
+            Assert.AreEqual("PAYPALISHIRING",result);
+        }
+
+        [Test]
+        public void Convert_2_Row()
+        {
+            var input = "PAYPALISHIRING";
+
+            var result = ZigZagConversion.Convert(input, 2);
+
+            Assert.AreEqual("PPIINAYALSHRIG",result);
+        }
+
+        [Test]
+        public void Convert_3_Row()
+        {
+            var input = "PAYPALISHIRING";
+
+            var result = ZigZagConversion.Convert(input, 3);
+
+            Assert.AreEqual("PAHNAPLSIIGYIR",result);
+        }
+
+        [Test]
+        public void Convert_4_Row()
+        {
+            var input = "PAYPALISHIRING";
+
+            var result = ZigZagConversion.Convert(input, 4);
+
+            Assert.AreEqual("PINAASRGYHPLII",result);
+        }
+    }
+}
---
+++ TDDArg/ZigZagConversion.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace TDDArg
+{
+    public class ZigZagConversion
+    {
+        public static string Convert(string text, int numRows)
+        {
+            if (numRows == 1) return text;
+
+            StringBuilder result = new StringBuilder();
+
+            for (int row = 0; row < numRows; row++)
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (row % 2 == 0)
+                    {
+                        if ((i - row) % (numRows + numRows / 2) == 0)
+                        {
+                            result.Append(text[i]);
+                        }
+                    }
+                    else
+                    {
+                        if (row == numRows - 1)
+                        {
+                            if ((i - row) % (numRows + numRows / 2) == 0)
+                            {
+                                result.Append(text[i]);
+                            }
+
+                            if ((i - row - numRows / 2) % (numRows + numRows / 2) == 0)
+                            {
+                                result.Append(text[i]);
+                            }
+                        }
+                        else
+                        {
+                            if ((i - row) % (numRows - 1) == 0)
+                            {
+                                result.Append(text[i]);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
