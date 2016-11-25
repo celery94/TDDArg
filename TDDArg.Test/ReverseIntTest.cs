80bc1f4e96464bdd640e5e283f5256670d24294c
Celery Liu <celery.liu@commscope.com>
2016-11-25 14:57:49 +0800

    Reverse Integer

---
+++ TDDArg.Test/ReverseIntTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class ReverseIntTest
+    {
+        [Test]
+        public void Reverse_One()
+        {
+            Assert.AreEqual(321, ReverseInt.Reverse(123));
+        }
+
+        [Test]
+        public void Reverse_Nagetive()
+        {
+            Assert.AreEqual(-321, ReverseInt.Reverse(-123));
+        }
+
+        [Test]
+        public void Reverse_10()
+        {
+            Assert.AreEqual(1, ReverseInt.Reverse(10));
+        }
+
+        [Test]
+        public void Reverse_Max()
+        {
+            Assert.AreEqual(0, ReverseInt.Reverse(1534236469));
+        }
+
+        [Test]
+        public void Reverse_Min()
+        {
+            Assert.AreEqual(0, ReverseInt.Reverse(-2147483648));
+        }
+    }
+}
---
+++ TDDArg/ReverseInt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TDDArg
+{
+    public class ReverseInt
+    {
+        public static int Reverse(int x)
+        {
+            int result = 0;
+
+            int last = x % 10;
+            while (x != 0)
+            {
+                if (x > 0 && (int.MaxValue - last) / 10 < result)
+                {
+                    return 0;
+                }
+
+                if (x < 0 && (int.MinValue / 10 - last / 10) > result)
+                {
+                    return 0;
+                }
+
+                result = result * 10 + last;
+                x = x / 10;
+                last = x % 10;
+            }
+
+            return result;
+        }
+    }
+}
