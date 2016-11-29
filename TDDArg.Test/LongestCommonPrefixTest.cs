07715553343bba87826771e0308f664539392cd7
Celery Liu <celery.liu@commscope.com>
2016-11-29 22:23:26 +0800

    Longest Common Prefix  passed

---
+++ TDDArg.Test/LongestCommonPrefixTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class LongestCommonPrefixTest
+    {
+        [Test]
+        public void When_empty_Then_whole_word()
+        {
+            Assert.AreEqual(string.Empty, LongestCommonPrefix.LongestCommonPrefixFunc(new string[] { }));
+        }
+
+        [Test]
+        public void When_single_Then_whole_word()
+        {
+            Assert.AreEqual("abcgd", LongestCommonPrefix.LongestCommonPrefixFunc(new[] { "abcgd" }));
+        }
+
+        [Test]
+        public void When_tow_Then_same_prefix()
+        {
+            Assert.AreEqual("ab", LongestCommonPrefix.LongestCommonPrefixFunc(new[] { "abcgd", "absadfsdaf" }));
+        }
+    }
+}
---
+++ TDDArg/LongestCommonPrefix.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace TDDArg
+{
+    public class LongestCommonPrefix
+    {
+        public static string LongestCommonPrefixFunc(string[] strs)
+        {
+            if (strs.Length == 0) return string.Empty;
+
+            StringBuilder result = new StringBuilder();
+
+            var index = 0;
+            while (index >= 0)
+            {
+                char ch = Char.MinValue;
+                for (int i = 0; i < strs.Length; i++)
+                {
+                    if (index > strs[i].Length - 1)
+                    {
+                        index = -2;
+                        break;
+                    }
+
+                    if (i == 0)
+                    {
+                        ch = strs[i][index];
+                    }
+                    else
+                    {
+                        if (strs[i][index] != ch)
+                        {
+                            index = -2;
+                            break;
+                        }
+                    }
+
+                    if (i == strs.Length - 1)
+                    {
+                        result.Append(ch);
+                    }
+                }
+                index++;
+            }
+
+            return result.ToString();
+        }
+    }
+}
