c66c1c14d51645a549461a58df29e7d2018e7b5b
Celery Liu <celery.liu@commscope.com>
2016-11-30 14:33:30 +0800

    ThreeSumClosest NotImplementedException

---
+++ TDDArg.Test/ThreeSumClosestTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class ThreeSumClosestTest
+    {
+        [Test]
+        [Ignore("")]
+        public void When_four_input_Then_2()
+        {
+            var nums = new int[] { -1, 2, 1, -4 };
+
+            Assert.AreEqual(2, ThreeSumClosest.ThreeSumClosestFunc(nums, 1));
+        }
+    }
+}
---
+++ TDDArg/ThreeSumClosest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDDArg
+{
+    public class ThreeSumClosest
+    {
+        public static int ThreeSumClosestFunc(int[] nums, int target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
