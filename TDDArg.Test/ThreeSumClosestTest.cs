b7af06e4005a2447102c53fd980cf4645d0b5966
Celery Liu <celery.liu@commscope.com>
2016-12-01 10:28:59 +0800

    ThreeSumClosest passed

--- TDDArg.Test/ThreeSumClosestTest.cs
+++ TDDArg.Test/ThreeSumClosestTest.cs
@@ -6,12 +6,43 @@ namespace TDDArg.Test
     public class ThreeSumClosestTest
     {
         [Test]
-        [Ignore("")]
+        public void When_0_0_0_Then_0()
+        {
+            var nums = new int[] { 0, 0, 0 };
+
+            Assert.AreEqual(0, ThreeSumClosest.ThreeSumClosestFunc(nums, 1));
+        }
+
+        [Test]
         public void When_four_input_Then_2()
         {
             var nums = new int[] { -1, 2, 1, -4 };
 
             Assert.AreEqual(2, ThreeSumClosest.ThreeSumClosestFunc(nums, 1));
+        }
+
+        [Test]
+        public void When_many_input_Then_4()
+        {
+            var nums = new int[] { -5, -4, -1, 1, 2, 2, 3, 7, 9 };
+
+            Assert.AreEqual(4, ThreeSumClosest.ThreeSumClosestFunc(nums, 4));
+        }
+
+        [Test]
+        public void When_1_1_1_0_Then_3()
+        {
+            var nums = new int[] { 1, 1, 1, 0 };
+
+            Assert.AreEqual(3, ThreeSumClosest.ThreeSumClosestFunc(nums, 100));
+        }
+
+        [Test]
+        public void When_result_not_have_start_and_end_Then_82()
+        {
+            var nums = new int[] { 1, 2, 4, 8, 16, 32, 64, 128 };
+
+            Assert.AreEqual(82, ThreeSumClosest.ThreeSumClosestFunc(nums, 82));
         }
     }
 }
--- TDDArg/ThreeSumClosest.cs
+++ TDDArg/ThreeSumClosest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace TDDArg
 {
@@ -6,7 +7,62 @@ namespace TDDArg
     {
         public static int ThreeSumClosestFunc(int[] nums, int target)
         {
-            throw new NotImplementedException();
+            nums = nums.OrderBy(q => q).ToArray();
+
+            int closest = nums[0] + nums[1] + nums[2];
+
+            for (int start = 0; start < nums.Length - 2; start++)
+            {
+                for (int end = nums.Length - 1; end > start + 1; end--)
+                {
+                    int middle = start + 1;
+
+                    while (middle < end)
+                    {
+                        if (nums[start] + nums[middle] + nums[end] >= target)
+                        {
+                            int currentCloest;
+
+                            if (middle == start + 1)
+                            {
+                                currentCloest = nums[start] + nums[middle] + nums[end];
+                            }
+                            else
+                            {
+                                if (nums[start] + nums[middle] + nums[end] - target > target - (nums[start] + nums[middle - 1] + nums[end]))
+                                {
+                                    currentCloest = nums[start] + nums[middle - 1] + nums[end];
+                                }
+                                else
+                                {
+                                    currentCloest = nums[start] + nums[middle] + nums[end];
+                                }
+                            }
+
+                            if (Math.Abs(target - closest) > Math.Abs(target - currentCloest))
+                            {
+                                closest = currentCloest;
+                            }
+
+                            break;
+                        }
+                        else
+                        {
+                            middle++;
+
+                            if (middle == end)
+                            {
+                                if (Math.Abs(target - closest) > Math.Abs(target - (nums[start] + nums[middle - 1] + nums[end])))
+                                {
+                                    closest = nums[start] + nums[middle - 1] + nums[end];
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return closest;
         }
     }
 }
