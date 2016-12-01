62347199dcc48486e8781ace3b0080ece0e8314f
Celery Liu <celery.liu@commscope.com>
2016-12-01 15:35:41 +0800

    LetterCombinationsOfAPhoneNumber

---
+++ TDDArg.Test/LetterCombinationsOfAPhoneNumberTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class LetterCombinationsOfAPhoneNumberTest
+    {
+        [Test]
+        public void When_Single_word_Then_char_array()
+        {
+            Assert.AreEqual(new List<string>() { "a", "b", "c" }, LetterCombinationsOfAPhoneNumber.LetterCombinations("2"));
+        }
+
+        [Test]
+        public void When_23_Then_combination_array()
+        {
+            var expect = new List<string>() {"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"};
+            var result = LetterCombinationsOfAPhoneNumber.LetterCombinations("23");
+
+            Assert.IsTrue(expect.Count==result.Count);
+            Assert.IsTrue(expect.Any(q=>result.Contains(q)));
+        }
+    }
+}
---
+++ TDDArg/LetterCombinationsOfAPhoneNumber.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TDDArg
+{
+    public class LetterCombinationsOfAPhoneNumber
+    {
+        public static IList<string> LetterCombinations(string digits)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>()
+            {
+                {"2","abc" },
+                {"3","def" },
+                {"4","ghi" },
+                {"5","jkl" },
+                {"6","mno" },
+                {"7","pqrs" },
+                {"8","tuv" },
+                {"9","wxyz" },
+            };
+
+            List<string> resultList = new List<string>();
+
+            foreach (var digit in digits)
+            {
+                if (resultList.Count == 0)
+                {
+                    resultList.AddRange(dictionary[digit.ToString()].ToCharArray().Select(q => q.ToString()));
+                }
+                else
+                {
+                    List<string> list = new List<string>();
+                    foreach (var ch in dictionary[digit.ToString()])
+                    {
+                        foreach (var item in resultList)
+                        {
+                            list.Add(item + ch);
+                        }
+                    }
+
+                    resultList = list;
+                }
+            }
+
+            return resultList;
+        }
+    }
+}
