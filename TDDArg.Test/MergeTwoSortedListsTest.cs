d5e8aa0a8ee2537b4e6ae9c9d9f447002cef65da
Celery Liu <celery.liu@commscope.com>
2016-12-02 14:30:21 +0800

    MergeTwoSortedLists passed

---
+++ TDDArg.Test/MergeTwoSortedListsTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    class MergeTwoSortedListsTest
+    {
+        [Test]
+        public void Given_one_node_list_is_empty_When_merged_Then_return_non_empty_node()
+        {
+            ListNode node1 = new ListNode(1);
+            ListNode node2 = null;
+
+            var node = MergeTwoSortedLists.MergeTwoLists(node1, node2);
+
+            List<int> result = new List<int>() { 1};
+            var index = 0;
+            while (node != null)
+            {
+                Assert.IsTrue(index < result.Count);
+                Assert.AreEqual(result[index], node.val);
+                node = node.next;
+                index++;
+            }
+        }
+
+        [Test]
+        public void Given_two_single_node_list_When_merged_Then_two_node()
+        {
+            ListNode node1 = new ListNode(1);
+            ListNode node2 = new ListNode(2);
+
+            var node = MergeTwoSortedLists.MergeTwoLists(node1, node2);
+
+            List<int> result = new List<int>() { 1, 2 };
+            var index = 0;
+            while (node != null)
+            {
+                Assert.IsTrue(index < result.Count);
+                Assert.AreEqual(result[index], node.val);
+                node = node.next;
+                index++;
+            }
+        }
+    }
+}
---
+++ TDDArg/MergeTwoSortedLists.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace TDDArg
+{
+    public class MergeTwoSortedLists
+    {
+        public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null && l2 == null)
+            {
+                return null;
+            }
+            if (l1 == null)
+            {
+                return l2;
+            }
+            if (l2 == null)
+            {
+                return l1;
+            }
+
+            ListNode head = null;
+            if (l1.val < l2.val)
+            {
+                head = l1;
+                l1 = l1.next;
+            }
+            else
+            {
+                head = l2;
+                l2 = l2.next;
+            }
+
+            var current = head;
+
+            while (l1 != null || l2 != null)
+            {
+                if (l1 == null)
+                {
+                    current.next = l2;
+                    l2 = null;
+                }
+                else if (l2 == null)
+                {
+                    current.next = l1;
+                    l1 = null;
+                }
+                else
+                {
+                    if (l1.val < l2.val)
+                    {
+                        current.next = l1;
+                        current = current.next;
+                        l1 = l1.next;
+                    }
+                    else
+                    {
+                        current.next = l2;
+                        current = current.next;
+                        l2 = l2.next;
+                    }
+                }
+            }
+
+            return head;
+        }
+    }
+}
