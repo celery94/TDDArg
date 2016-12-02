813d1ec513a1eeef1b04596512e53c6f089b8ba3
Celery Liu <celery.liu@commscope.com>
2016-12-02 13:46:43 +0800

    ValidParentheses passed
    using stack to check

---
+++ TDDArg.Test/ValidParenthesesTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class ValidParenthesesTest
+    {
+        [Test]
+        public void When_only_one_pair_Then_valid()
+        {
+            Assert.AreEqual(true, ValidParentheses.IsValid("()"));
+        }
+
+        [Test]
+        public void When_two_pair_Then_valid()
+        {
+            Assert.AreEqual(true, ValidParentheses.IsValid("(){}"));
+        }
+
+        [Test]
+        public void When_pair_contain_in_Then_valid()
+        {
+            Assert.AreEqual(true, ValidParentheses.IsValid("([]){}"));
+        }
+
+        [Test]
+        public void When_single_Then_invalid()
+        {
+            Assert.AreEqual(false, ValidParentheses.IsValid("("));
+        }
+
+        [Test]
+        public void When_single_end_Then_invalid()
+        {
+            Assert.AreEqual(false, ValidParentheses.IsValid(")"));
+        }
+
+        [Test]
+        public void When_without_end_Then_invalid()
+        {
+            Assert.AreEqual(false, ValidParentheses.IsValid("({)"));
+        }
+
+        [Test]
+        public void When_pair_crossed_Then_invalid()
+        {
+            Assert.AreEqual(false, ValidParentheses.IsValid("({)}"));
+        }
+    }
+}
---
+++ TDDArg/ValidParentheses.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace TDDArg
+{
+    public class ValidParentheses
+    {
+        public static bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var ch in s.ToCharArray())
+            {
+                if (ch == '(' || ch == '[' || ch == '{')
+                {
+                    stack.Push(ch);
+                }
+
+                if (ch == ')')
+                {
+                    if (stack.Count > 0 && stack.Peek() == '(')
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                if (ch == ']')
+                {
+                    if (stack.Count > 0 && stack.Peek() == '[')
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                if (ch == '}')
+                {
+                    if (stack.Count > 0 && stack.Peek() == '{')
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
