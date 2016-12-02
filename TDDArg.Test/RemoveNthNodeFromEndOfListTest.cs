0b09e04be419aca0e457dde4d960ba2f9f2953bb
Celery Liu <celery.liu@commscope.com>
2016-12-02 13:04:48 +0800

    Remove Nth Node From End of List

---
+++ TDDArg.Test/RemoveNthNodeFromEndOfListTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class RemoveNthNodeFromEndOfListTest
+    {
+        [Test]
+        public void Given_5_linked_node_When_remove_2th_from_end_of_List_Then_2th_from_end_removed()
+        {
+            ListNode node = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                        {
+                            next = new ListNode(5)
+                        }
+                    }
+                }
+            };
+
+            var result = RemoveNthNodeFromEndOfList.RemoveNthFromEnd(node, 2);
+
+
+            while (result != null)
+            {
+                Assert.AreNotEqual(4, result.val);
+                result = result.next;
+            }
+        }
+
+        [Test]
+        public void Given_1_linked_node_When_remove_1th_from_end_of_List_Then_empty()
+        {
+            ListNode node = new ListNode(1)
+            {
+
+            };
+
+            var result = RemoveNthNodeFromEndOfList.RemoveNthFromEnd(node, 1);
+
+            Assert.AreEqual(null, result);
+        }
+    }
+}
---
+++ TDDArg/RemoveNthNodeFromEndOfList.cs
@@ -0,0 +1,33 @@
+﻿namespace TDDArg
+{
+    public class RemoveNthNodeFromEndOfList
+    {
+        public static ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var end = head;
+            while (n > 0)
+            {
+                if (end == null) return null;
+
+                end = end.next;
+                n--;
+            }
+
+            if (end == null)
+            {
+                return head.next;
+            }
+
+            var removeNode = head;
+            while (end.next != null)
+            {
+                removeNode = removeNode.next;
+                end = end.next;
+            }
+
+            removeNode.next = removeNode.next.next;
+
+            return head;
+        }
+    }
+}
