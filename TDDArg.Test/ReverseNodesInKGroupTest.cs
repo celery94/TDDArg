3827458c8a96f20b438fc4e3c4ca0510949bfb97
Celery Liu <liuct94@gmail.com>
2016-12-03 13:32:24 +0800

    Reverse Nodes in k-Group passed

---
+++ TDDArg.Test/ReverseNodesInKGroupTest.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class ReverseNodesInKGroupTest
+    {
+        private ListNode _node = null;
+
+        [SetUp]
+        public void Prepare()
+        {
+            _node = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                        {
+                            next = new ListNode(5)
+                            {
+                                next = new ListNode(6)
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+        [Test]
+        public void Given_6_node_list_When_k_is_6_Then_return()
+        {
+            var result = ReverseNodesInKGroup.ReverseKGroup(_node, 6);
+
+            var vals = new Queue<int>(new[] {6, 5, 4, 3, 2, 1});
+
+            while (result != null)
+            {
+                Assert.AreEqual(vals.Dequeue(), result.val);
+                result = result.next;
+            }
+        }
+
+        [Test]
+        public void Given_6_node_list_When_k_is_3_Then_return()
+        {
+            var result = ReverseNodesInKGroup.ReverseKGroup(_node, 3);
+
+            var vals = new Queue<int>(new[] {3, 2, 1, 6, 5, 4});
+
+            while (result != null)
+            {
+                Assert.AreEqual(vals.Dequeue(), result.val);
+                result = result.next;
+            }
+        }
+
+        [Test]
+        public void Given_6_node_list_When_k_is_4_Then_return()
+        {
+            var result = ReverseNodesInKGroup.ReverseKGroup(_node, 4);
+
+            var vals = new Queue<int>(new[] {4, 3, 2, 1, 5, 6});
+
+            while (result != null)
+            {
+                Assert.AreEqual(vals.Dequeue(), result.val);
+                result = result.next;
+            }
+        }
+    }
+}
---
+++ TDDArg/ReverseNodesInKGroup.cs
@@ -0,0 +1,57 @@
+﻿namespace TDDArg
+{
+    public class ReverseNodesInKGroup
+    {
+        public static ListNode ReverseKGroup(ListNode head, int k)
+        {
+            ListNode current = null;
+
+            while (head != null)
+            {
+                ListNode reversNode = null;
+                int count = k;
+
+                var cur = head;
+                var nodeCount = 0;
+                while (cur != null)
+                {
+                    nodeCount++;
+                    cur = cur.next;
+                }
+
+                if (nodeCount < count)
+                {
+                    reversNode = head;
+                    head = null;
+                }
+                else
+                {
+                    while (head != null && count > 0)
+                    {
+                        var next = head.next;
+                        head.next = reversNode;
+                        reversNode = head;
+                        head = next;
+                        count--;
+                    }
+                }
+
+                if (current == null)
+                {
+                    current = reversNode;
+                }
+                else
+                {
+                    var node = current;
+                    while (node.next != null)
+                    {
+                        node = node.next;
+                    }
+
+                    node.next = reversNode;
+                }
+            }
+            return current;
+        }
+    }
+}
