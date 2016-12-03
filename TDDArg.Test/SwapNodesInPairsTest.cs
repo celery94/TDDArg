0eb801fea7d96e36130ffc0bd316923f1ce9999b
Celery Liu <liuct94@gmail.com>
2016-12-03 11:02:27 +0800

    SwapNodesInPairs passed

---
+++ TDDArg.Test/SwapNodesInPairsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class SwapNodesInPairsTest
+    {
+        [Test]
+        public void Given_list_node_When_swap_Then_return()
+        {
+            ListNode node = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                    }
+                }
+            };
+
+            var result = SwapNodesInPairs.SwapPairs(node);
+
+            Assert.AreEqual(2, result.val);
+            Assert.AreEqual(1, result.next.val);
+            Assert.AreEqual(4, result.next.next.val);
+            Assert.AreEqual(3, result.next.next.next.val);
+        }
+    }
+}
---
+++ TDDArg/SwapNodesInPairs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TDDArg
+{
+    public class SwapNodesInPairs
+    {
+        public static ListNode SwapPairs(ListNode head)
+        {
+            if (head?.next == null) return head;
+
+            var next = head.next;
+            head.next = next.next;
+            next.next = head;
+
+            head.next = SwapPairs(head.next);
+
+            return next;
+        }
+    }
+}
