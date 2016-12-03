58456f21bf63f2a71ed1adb0534bf7839622cc16
Celery Liu <liuct94@gmail.com>
2016-12-03 10:15:07 +0800

    GenerateParentheses
    using recursion need review and think about this solution

---
+++ TDDArg.Test/GenerateParenthesesTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class GenerateParenthesesTest
+    {
+        [Test]
+        public void Given_number_1_When_gen_Then_one_result()
+        {
+            Assert.AreEqual(new List<string> {"()"}, GenerateParentheses.GenerateParenthesis(1));
+        }
+
+        [Test]
+        public void Given_number_2_When_gen_Then_two_result()
+        {
+            var expect = new List<string>()
+            {
+                "()()",
+                "(())"
+            };
+
+            var result = GenerateParentheses.GenerateParenthesis(2);
+            Assert.AreEqual(expect.Count, result.Count);
+        }
+    }
+}
---
+++ TDDArg/GenerateParentheses.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDArg
+{
+    public class GenerateParentheses
+    {
+        public static IList<string> GenerateParenthesis(int n)
+        {
+            List<string> result = new List<string>();
+
+            AppendString(result, "", 0, 0, n);
+
+            return result;
+        }
+
+        private static void AppendString(List<string> result, string sb, int open, int close, int max)
+        {
+            if (sb.Length == max * 2)
+            {
+                result.Add(sb.ToString());
+                return;
+            }
+
+            if (open < max)
+                AppendString(result, sb + "(", open + 1, close, max);
+
+            if (close < open)
+                AppendString(result, sb + ")", open, close + 1, max);
+        }
+    }
+}
