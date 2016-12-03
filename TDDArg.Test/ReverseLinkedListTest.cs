358d38cf588fd08fefb9d0464431c0fb995de1fb
Celery Liu <liuct94@gmail.com>
2016-12-03 12:45:05 +0800

    ReverseLinkedList passed

---
+++ TDDArg.Test/ReverseLinkedListTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class ReverseLinkedListTest
+    {
+        [Test]
+        public void Given_list_When_reverse_Then_return()
+        {
+            ListNode node = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                    }
+                }
+            };
+
+            var result = ReverseLinkedList.ReverseList(node);
+
+            Assert.AreEqual(4, result.val);
+            Assert.AreEqual(3, result.next.val);
+            Assert.AreEqual(2, result.next.next.val);
+            Assert.AreEqual(1, result.next.next.next.val);
+        }
+    }
+}
---
+++ TDDArg/ReverseLinkedList.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlTypes;
+
+namespace TDDArg
+{
+    public class ReverseLinkedList
+    {
+        public static ListNode ReverseList(ListNode head)
+        {
+            if (head?.next == null) return head;
+
+            ListNode current = null;
+
+            while (head != null)
+            {
+                var next = head.next;
+                head.next = current;
+
+                current = head;
+                head = next;
+            }
+
+            return current;
+        }
+    }
+}
