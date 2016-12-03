f651363d03c7d71ae407a08a134774734189da08
Celery Liu <liuct94@gmail.com>
2016-12-03 14:28:37 +0800

    28. Implement strStr passed

---
+++ TDDArg.Test/ImplementStrStrTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class ImplementStrStrTest
+    {
+        [Test]
+        public void Given_two_string_When_search_Then_return_index()
+        {
+            Assert.AreEqual(0, ImplementStrStr.StrStr("abcde", "abc"));
+        }
+    }
+}
---
+++ TDDArg/ImplementStrStr.cs
@@ -0,0 +1,32 @@
+﻿namespace TDDArg
+{
+    public class ImplementStrStr
+    {
+        public static int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrEmpty(needle)) return 0;
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                for (int j = 0; j < needle.Length; j++)
+                {
+                    if (i + j >= haystack.Length)
+                    {
+                        return -1;
+                    }
+
+                    if (haystack[i + j] != needle[j])
+                    {
+                        break;
+                    }
+                    else if (j == needle.Length - 1)
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
