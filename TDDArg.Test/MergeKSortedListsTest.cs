eff4b69915bf2f7ce024ae55ff287d4c497db806
Celery Liu <liuct94@gmail.com>
2016-12-03 10:45:16 +0800

    MergeKSortedLists passed

---
+++ TDDArg.Test/MergeKSortedListsTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class MergeKSortedListsTest
+    {
+        [Test]
+        public void Given_2_linked_list_When_merged_Then_return()
+        {
+            ListNode node1 = new ListNode(1)
+            {
+                next = new ListNode(3)
+            };
+            ListNode node2 = new ListNode(2)
+            {
+                next = new ListNode(4)
+            };
+
+            var result = MergeKSortedLists.MergeKLists(new ListNode[] {node1, node2});
+
+            Assert.AreEqual(1, result.val);
+            Assert.AreEqual(2, result.next.val);
+            Assert.AreEqual(3, result.next.next.val);
+            Assert.AreEqual(4, result.next.next.next.val);
+        }
+    }
+}
---
+++ TDDArg/MergeKSortedLists.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TDDArg
+{
+    public class MergeKSortedLists
+    {
+        public static ListNode MergeKLists(ListNode[] lists)
+        {
+            if (!lists.Any() || lists.All(q => q == null)) return null;
+
+            int minVal = Int32.MaxValue;
+            int index = -1;
+            for (int i = 0; i < lists.Length; i++)
+            {
+                if (lists[i] != null)
+                {
+                    if (lists[i].val < minVal)
+                    {
+                        minVal = lists[i].val;
+                        index = i;
+                    }
+                }
+            }
+
+            var head = lists[index];
+            lists[index] = lists[index].next;
+
+            var current = head;
+            while (lists.Any(q => q != null))
+            {
+                minVal = Int32.MaxValue;
+                index = -1;
+                for (int i = 0; i < lists.Length; i++)
+                {
+                    if (lists[i] != null)
+                    {
+                        if (lists[i].val < minVal)
+                        {
+                            minVal = lists[i].val;
+                            index = i;
+                        }
+                    }
+                }
+
+                current.next = lists[index];
+                current = current.next;
+                lists[index] = lists[index].next;
+            }
+
+            return head;
+        }
+    }
+}
