182cf4c14160adf61573f7c8687c93ce269950f4
Celery Liu <liuct94@gmail.com>
2016-12-03 14:18:15 +0800

    Remove Element Of Array passed

---
+++ TDDArg.Test/RemoveElementOfArrayTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class RemoveElementOfArrayTest
+    {
+        [Test]
+        public void Given_array_When_remove_element_Then_return()
+        {
+            var nums = new int[] {3, 2, 2, 3};
+
+            var result = RemoveElementOfArray.RemoveElement(nums, 3);
+
+            var expect = new int[] {2, 2};
+
+            Assert.AreEqual(2, result);
+            for (int i = 0; i < expect.Length; i++)
+            {
+                Assert.AreEqual(expect[i], nums[i]);
+            }
+        }
+    }
+}
---
+++ TDDArg/RemoveElementOfArray.cs
@@ -0,0 +1,20 @@
+﻿namespace TDDArg
+{
+    public class RemoveElementOfArray
+    {
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int index = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[index] = nums[i];
+                    index++;
+                }
+            }
+
+            return index;
+        }
+    }
+}
