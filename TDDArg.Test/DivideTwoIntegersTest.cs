bb91947d4c4176e72f48d04f059753e633ef14ec
Celery Liu <celery.liu@commscope.com>
2016-12-05 11:26:39 +0800

    Divide Two Integers passed

---
+++ TDDArg.Test/DivideTwoIntegersTest.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class DivideTwoIntegersTest
+    {
+        [Test]
+        public void Given_101_When_divide_0_Then_int_max()
+        {
+            Assert.AreEqual(int.MaxValue, DivideTwoIntegers.Divide(101, 0));
+        }
+
+        [Test]
+        public void Given_int_min_When_divide_negative_1_Then_int_max()
+        {
+            Assert.AreEqual(int.MaxValue, DivideTwoIntegers.Divide(int.MinValue, -1));
+        }
+
+        [Test]
+        public void Given_101_When_divide_max_than_101_Then_zero()
+        {
+            Assert.AreEqual(0, DivideTwoIntegers.Divide(101, 202));
+        }
+
+        [Test]
+        public void Given_n101_When_divide_min_than_101_Then_zero()
+        {
+            Assert.AreEqual(0, DivideTwoIntegers.Divide(-101, -202));
+        }
+
+        [Test]
+        public void Given_20_When_divide_5_Then_4()
+        {
+            Assert.AreEqual(4, DivideTwoIntegers.Divide(20, 5));
+        }
+
+        [Test]
+        public void Given_20_When_divide_3_Then_6()
+        {
+            Assert.AreEqual(6, DivideTwoIntegers.Divide(20, 3));
+        }
+
+        [Test]
+        public void Given_10_When_divide_10_Then_1()
+        {
+            Assert.AreEqual(1, DivideTwoIntegers.Divide(10, 10));
+        }
+
+        [Test]
+        public void Given_2147483647_When_divide_2_Then_1073741823()
+        {
+            Assert.AreEqual(1073741823, DivideTwoIntegers.Divide(2147483647, 2));
+        }
+
+        [Test]
+        public void Given_1100540749_When_divide_n1090366779_Then_n1()
+        {
+            Assert.AreEqual(-1, DivideTwoIntegers.Divide(1100540749, -1090366779));
+        }
+    }
+}
---
+++ TDDArg/DivideTwoIntegers.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace TDDArg
+{
+    public class DivideTwoIntegers
+    {
+        public static int Divide(int dividend, int divisor)
+        {
+            if (divisor == 0 || (dividend == int.MinValue && divisor == -1)) return int.MaxValue;
+
+            if (divisor == 1)
+            {
+                return dividend;
+            }
+            if (divisor == -1)
+            {
+                return 0 - dividend;
+            }
+
+            bool isNagetive = dividend < 0;
+            bool isNagetiveDiv = divisor < 0;
+
+            if (!isNagetive)
+            {
+                dividend = 0 - dividend;
+            }
+            if (!isNagetiveDiv)
+            {
+                divisor = 0 - divisor;
+            }
+
+            if (divisor < dividend) return 0;
+
+            int result = 0;
+
+            while (dividend <= divisor)
+            {
+                int index = 1;
+                var divisorSum = divisor;
+
+                dividend = dividend - divisorSum;
+                while (dividend <= 0)
+                {
+                    result += index;
+
+                    if (divisorSum < int.MinValue - divisorSum)
+                    {
+                        break;
+                    }
+
+                    divisorSum += divisorSum; //May min that int.MinValue
+                    index += index;
+
+                    if (dividend - divisorSum <= 0)
+                    {
+                        dividend = dividend - divisorSum;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (isNagetive != isNagetiveDiv)
+            {
+                return 0 - result;
+            }
+            else
+            {
+                return result;
+            }
+        }
+    }
+}
