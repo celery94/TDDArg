f4a0b43c9da3b75ef548695d3905f579f6deefde
Celery Liu <celery.liu@commscope.com>
2016-12-06 16:11:23 +0800

    SubstringWithConcatenationOfAllWords with performance issue

---
+++ TDDArg.Test/SubstringWithConcatenationOfAllWordsTest.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class SubstringWithConcatenationOfAllWordsTest
+    {
+        [Test]
+        public void Given_string_When_find_one_word_Then_return()
+        {
+            var s = "barfoothefoobarman";
+            string[] words = new[] { "foo" };
+
+            var result = SubstringWithConcatenationOfAllWords.FindSubstring(s, words);
+
+            Assert.AreEqual(new List<int> { 3, 9 }, result);
+        }
+
+        [Test]
+        public void Given_string_When_find_two_word_Then_return()
+        {
+            var s = "barfoothefoobarman";
+            string[] words = new[] { "foo", "bar" };
+
+            var result = SubstringWithConcatenationOfAllWords.FindSubstring(s, words);
+
+            Assert.AreEqual(new List<int> { 0, 9 }, result);
+        }
+
+        [Test]
+        public void Given_string_When_find_three_word_Then_return()
+        {
+            var s = "barfoofoobarthefoobarman";
+            string[] words = new[] { "foo", "bar", "the" };
+
+            var result = SubstringWithConcatenationOfAllWords.FindSubstring(s, words);
+            var expect = new List<int> { 6, 9, 12 };
+
+
+            Assert.AreEqual(expect.Count, result.Count);
+            Assert.IsTrue(expect.Any(q => result.Contains(q)));
+        }
+
+        [Test]
+        public void Given_string_When_find_three_word_Then_return_0()
+        {
+            var s = "foothebar";
+            string[] words = new[] { "foo", "bar", "the" };
+
+            var result = SubstringWithConcatenationOfAllWords.FindSubstring(s, words);
+
+            Assert.AreEqual(new List<int> { 0 }, result);
+        }
+
+        [Test]
+        public void Given_string_When_find_4_word_duplicate_Then_return()
+        {
+            var s = "wordgoodgoodgoodbestword";
+            string[] words = new[] { "word", "good", "best", "good" };
+
+            var result = SubstringWithConcatenationOfAllWords.FindSubstring(s, words);
+
+            Assert.AreEqual(new List<int> { 8 }, result);
+        }
+    }
+}
---
+++ TDDArg/SubstringWithConcatenationOfAllWords.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace TDDArg
+{
+    public class SubstringWithConcatenationOfAllWords
+    {
+        public static IList<int> FindSubstring(string s, string[] words)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                var word = words[i];
+                int index = s.IndexOf(word);
+                while (index != -1)
+                {
+                    int preIndex = index;
+                    int nextIndex = index + word.Length;
+
+                    if (words.Length > 1)
+                    {
+                        FindString(s, result, words, preIndex, nextIndex, new List<int>() { i });
+                    }
+                    else
+                    {
+                        result.Add(index);
+                    }
+
+                    index = s.IndexOf(word, index + 1);
+                }
+            }
+
+            return result;
+        }
+
+        private static void FindString(string s, List<int> result, string[] words, int preIndex, int nextIndex, List<int> wordsUsed)
+        {
+            if (words.Length <= wordsUsed.Count) return;
+
+            for (int index = 0; index < words.Length; index++)
+            {
+                if (wordsUsed.Contains(index)) continue;
+
+                var word = words[index];
+                if (nextIndex + word.Length - 1 < s.Length && s.Substring(nextIndex, word.Length) == word)
+                {
+                    List<int> used = new List<int>();
+                    used.AddRange(wordsUsed);
+                    used.Add(index);
+
+                    if (words.Length <= used.Count)
+                    {
+                        if (!result.Contains(preIndex))
+                            result.Add(preIndex);
+                    }
+                    else
+                    {
+                        FindString(s, result, words, preIndex, nextIndex + word.Length, used);
+                    }
+                }
+
+                if (preIndex - word.Length >= 0 && s.Substring(preIndex - word.Length, word.Length) == word)
+                {
+                    List<int> used = new List<int>();
+                    used.AddRange(wordsUsed);
+                    used.Add(index);
+
+                    if (words.Length <= used.Count)
+                    {
+                        if (!result.Contains(preIndex - word.Length))
+                            result.Add(preIndex - word.Length);
+                    }
+                    else
+                    {
+                        FindString(s, result, words, preIndex - word.Length, nextIndex, used);
+                    }
+                }
+            }
+        }
+    }
+}
