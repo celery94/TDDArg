42f0e2b309400084835e5ee292cea365731592c2
Celery Liu <celery.liu@commscope.com>
2016-12-07 11:02:43 +0800

    32. Longest Valid Parentheses with performance issue

---
+++ TDDArg.Test/LongestValidParenthesesTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class LongestValidParenthesesTest
+    {
+        [Test]
+        public void Given_three_When_find_Then_return_2()
+        {
+            Assert.AreEqual(2, LongestValidParentheses.FindLongestValidParentheses("(()"));
+        }
+
+        [Test]
+        public void Given_6_When_find_Then_return_4()
+        {
+            Assert.AreEqual(4, LongestValidParentheses.FindLongestValidParentheses(")()())"));
+        }
+    }
+}
---
+++ TDDArg/LongestValidParentheses.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace TDDArg
+{
+    public class LongestValidParentheses
+    {
+        public static int FindLongestValidParentheses(string s)
+        {
+            int maxLength = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                Queue<char> queue = new Queue<char>();
+                int j = i;
+                while (j < s.Length)
+                {
+                    if (queue.Count > 0 && s[j] == ')' && queue.Peek() == '(')
+                    {
+                        queue.Dequeue();
+                        if (queue.Count == 0 && (j - i + 1) > maxLength)
+                        {
+                            maxLength = j - i + 1;
+                        }
+                    }
+                    else
+                    {
+                        queue.Enqueue(s[j]);
+                    }
+                    j++;
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
