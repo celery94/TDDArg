d591972362d0ce0d6f965e2fbd2f07093f3a767d
Celery Liu <celery.liu@commscope.com>
2016-12-07 16:05:01 +0800

    ValidSudoku NotImplemented

---
+++ TDDArg.Test/ValidSudokuTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class ValidSudokuTest
+    {
+        [Test]
+        public void Given_array_When_validate_Then_return()
+        {
+            
+        }
+    }
+}
---
+++ TDDArg/ValidSudoku.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace TDDArg
+{
+    public class ValidSudoku
+    {
+        public static bool IsValidSudoku(char[,] board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
