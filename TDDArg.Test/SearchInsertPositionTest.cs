26796c5244f3eee5bd3e9cb57dc9648e189cb076
Celery Liu <celery.liu@commscope.com>
2016-12-07 15:30:09 +0800

    35. Search Insert Position passed

---
+++ TDDArg.Test/SearchInsertPositionTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class SearchInsertPositionTest
+    {
+        [Test]
+        public void Given_array_When_search_found_Then_middle()
+        {
+            var nums = new int[] { 1, 3, 5, 6 };
+
+            Assert.AreEqual(2, SearchInsertPosition.SearchInsert(nums, 5));
+        }
+
+        [Test]
+        public void Given_array_When_search_Then_middle()
+        {
+            var nums = new int[] { 1, 3, 5, 6 };
+
+            Assert.AreEqual(1, SearchInsertPosition.SearchInsert(nums, 2));
+        }
+
+        [Test]
+        public void Given_array_When_search_Then_first()
+        {
+            var nums = new int[] { 1, 3, 5};
+
+            Assert.AreEqual(0, SearchInsertPosition.SearchInsert(nums, 1));
+        }
+
+        [Test]
+        public void Given_single_array_When_search_Then_return()
+        {
+            var nums = new int[] { 1 };
+
+            Assert.AreEqual(1, SearchInsertPosition.SearchInsert(nums, 2));
+        }
+        [Test]
+        public void Given_array_When_search_Then_return_end()
+        {
+            var nums = new int[] { 1,3 };
+
+            Assert.AreEqual(1, SearchInsertPosition.SearchInsert(nums, 3));
+        }
+    }
+}
---
+++ TDDArg/SearchInsertPosition.cs
@@ -0,0 +1,46 @@
+﻿namespace TDDArg
+{
+    public class SearchInsertPosition
+    {
+        public static int SearchInsert(int[] nums, int target)
+        {
+            int start = 0, end = nums.Length - 1;
+
+            if (target <= nums[start]) return 0;
+            if (target > nums[end]) return end + 1;
+            if (target == nums[end]) return end;
+
+            while (start <= end)
+            {
+                var mid = start + (end - start) / 2;
+
+                if (nums[mid] < target)
+                {
+                    start = mid;
+                }
+                else if (nums[mid] > target)
+                {
+                    end = mid;
+                }
+                else
+                {
+                    return mid;
+                }
+
+                if (start == end - 1)
+                {
+                    if (nums[start] == target)
+                    {
+                        return start;
+                    }
+                    else
+                    {
+                        return end;
+                    }
+                }
+            }
+
+            return end;
+        }
+    }
+}
