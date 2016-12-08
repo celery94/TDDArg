380611260d61d838a411f4ab7901a0cbef1e17c1
Celery Liu <celery.liu@commscope.com>
2016-12-08 14:37:15 +0800

    CombinationSum

--- TDDArg/CombinationSum.cs
+++ TDDArg/CombinationSum.cs
@@ -11,24 +11,24 @@ namespace TDDArg
         {
             List<IList<int>> result = new List<IList<int>>();
             candidates = candidates.OrderBy(q => q).ToArray();
-            Find(result, new List<int>(), candidates, target);
+            Find(result, new List<int>(), candidates, target, 0);
             return result;
         }
 
-        private static void Find(List<IList<int>> result, List<int> list, int[] candidates, int target)
+        private static void Find(List<IList<int>> result, List<int> list, int[] candidates, int target, int start)
         {
             if (target == 0)
             {
-                //TODO check duplicate
                 result.Add(new List<int>(list));
             }
 
-            foreach (var candidate in candidates)
+            for (int index = start; index < candidates.Length; index++)
             {
+                var candidate = candidates[index];
                 if (candidate <= target)
                 {
                     list.Add(candidate);
-                    Find(result, list, candidates, target - candidate);
+                    Find(result, list, candidates, target - candidate, index);
                     list.RemoveAt(list.Count - 1);
                 }
             }
