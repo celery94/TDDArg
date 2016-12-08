1b7e38473f3c250c88f2590e212f6ab7f73f0aeb
Celery Liu <celery.liu@commscope.com>
2016-12-08 12:34:09 +0800

    CombinationSum with performance issue

---
+++ TDDArg.Test/CombinationSumTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class CombinationSumTest
+    {
+        [Test]
+        public void Given_array_When_find_Then_return()
+        {
+            var nums = new int[] { 2, 6, 7 };
+
+            var expectResult = new List<List<int>>()
+            {
+                new List<int>() {7}
+            };
+
+            var result = CombinationSum.CombinationSumFunc(nums, 7);
+
+            Assert.AreEqual(expectResult.Count, result.Count);
+        }
+    }
+}
---
+++ TDDArg/CombinationSum.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TDDArg
+{
+    public class CombinationSum
+    {
+        public static IList<IList<int>> CombinationSumFunc(int[] candidates, int target)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+            Find(result, new List<int>(), candidates, target);
+            return result;
+        }
+
+        private static void Find(List<IList<int>> result, List<int> list, int[] candidates, int target)
+        {
+            foreach (var candidate in candidates)
+            {
+                List<int> array = new List<int>();
+                array.AddRange(list);
+                array.Add(candidate);
+
+                if (array.Sum() == target)
+                {
+                    array = array.OrderBy(q => q).ToList();
+
+                    if (!result.Where(q => q.Count == array.Count).Any(q =>
+                         {
+                             for (int i = 0; i < q.Count; i++)
+                             {
+                                 if (q[i] != array[i])
+                                 {
+                                     return false;
+                                 }
+                             }
+
+                             return true;
+                         }))
+                        result.Add(array);
+                }
+                else if (array.Sum() < target)
+                {
+                    Find(result, array, candidates, target);
+                }
+            }
+        }
+    }
+}
