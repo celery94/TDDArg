62c19a3314d55ffb0f65b4f24f66edcaead69d78
Celery Liu <celery.liu@commscope.com>
2016-12-09 13:23:15 +0800

    41. First Missing Positive passed

---
+++ TDDArg.Test/FirstMissingPositiveTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class FirstMissingPositiveTest
+    {
+        [Test]
+        public void Given_array_When_find_Then_return()
+        {
+            Assert.AreEqual(1, FirstMissingPositive.FindFirstMissingPositive(new int[] { }));
+            Assert.AreEqual(2, FirstMissingPositive.FindFirstMissingPositive(new int[] { 1 }));
+            Assert.AreEqual(2, FirstMissingPositive.FindFirstMissingPositive(new int[] { 1, 1 }));
+            Assert.AreEqual(3, FirstMissingPositive.FindFirstMissingPositive(new int[] { 1, 2, 0 }));
+            Assert.AreEqual(2, FirstMissingPositive.FindFirstMissingPositive(new int[] { 3, 4, -1, 1 }));
+        }
+    }
+}
---
+++ TDDArg/FirstMissingPositive.cs
@@ -0,0 +1,43 @@
+﻿namespace TDDArg
+{
+    public class FirstMissingPositive
+    {
+        public static int FindFirstMissingPositive(int[] nums)
+        {
+            //0～1，1～2，2～3
+            //遍历已有数组，找到当前的数，如果在int范围内（1～N），则替换到该位置
+            //最后挨个检查，位置上的数不对的 就是缺的
+
+            int i = 0;
+
+            while (i < nums.Length)
+            {
+                var current = nums[i];
+                if (current <= 0 || current > nums.Length || (current == i + 1))
+                {
+                    i++;
+                }
+                else if (nums[current - 1] != current)
+                {
+                    var temp = nums[current - 1];
+                    nums[current - 1] = current;
+                    nums[i] = temp;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            for (int index = 0; index < nums.Length; index++)
+            {
+                if (nums[index] != index + 1)
+                {
+                    return index + 1;
+                }
+            }
+
+            return nums.Length + 1;
+        }
+    }
+}
