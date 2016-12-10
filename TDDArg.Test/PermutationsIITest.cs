00062a32a16e5d0b7b6aa88976aaaedab1b4d8aa
Celery Liu <liuct94@gmail.com>
2016-12-10 12:32:19 +0800

    47. Permutations II with issue

---
+++ TDDArg.Test/PermutationsIITest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class PermutationsIITest
+	{
+		[Test]
+		[Ignore("")]
+		public void Given_array_When_permute_Then_return()
+		{
+			var nums = new int[] { 1, 1, 2, 2 };
+
+			var result = PermutationsII.PermuteUnique(nums);
+
+			foreach (var item in result)
+			{
+				Console.WriteLine(string.Join(",", item));
+			}
+
+			Assert.AreEqual(6, result.Count);
+		}
+	}
+}
---
+++ TDDArg/PermutationsII.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TDDArg
+{
+	public class PermutationsII
+	{
+		public static IList<IList<int>> PermuteUnique(int[] nums)
+		{
+			nums = nums.OrderBy(q => q).ToArray();
+
+			List<IList<int>> result = new List<IList<int>>();
+
+			Find(result, nums, new List<int> {  });
+
+			return result;
+		}
+
+		static void Find(List<IList<int>> result, int[] nums, List<int> currentList)
+		{
+			if (currentList.Count == nums.Length)
+			{
+				result.Add(currentList.Select(q => nums[q]).ToList());
+				return;
+			}
+
+			int? pre = null;
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if (!currentList.Contains(i))
+				{
+					var item = nums[i];
+
+					if (pre == null) pre = item;
+					else if (pre == item) continue;
+
+					currentList.Add(i);
+					Find(result, nums, currentList);
+					currentList.RemoveAt(currentList.Count - 1);
+				}
+			}
+		}
+	}
+}
