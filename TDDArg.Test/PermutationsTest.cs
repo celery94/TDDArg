f422f135e456f2d54add49106f72089718416f3e
Celery Liu <liuct94@gmail.com>
2016-12-10 11:22:50 +0800

    46. Permutations passed

---
+++ TDDArg.Test/PermutationsTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class PermutationsTest
+	{
+		[Test]
+		public void Given_2_items_array_When_permute_Then_return()
+		{
+			var nums = new int[] { 2, 3 };
+
+			var expectResult = new List<IList<int>>
+			{
+				new List<int>{2,3},
+				new List<int>{3,2},
+			};
+
+			Assert.AreEqual(2, Permutations.Permute(nums).Count);
+		}
+
+		[Test]
+		public void Given_3_items_array_When_permute_Then_return()
+		{
+			var nums = new int[] {1, 2, 3 };
+
+			var expectResult = new List<IList<int>>
+			{
+				new List<int>{2,3},
+				new List<int>{3,2},
+			};
+
+			Assert.AreEqual(6, Permutations.Permute(nums).Count);
+		}
+	}
+}
