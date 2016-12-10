0999f47c72acd2e8942b408853bd6f2401ce5de7
Celery Liu <liuct94@gmail.com>
2016-12-10 10:10:13 +0800

    43. Multiply Strings passed
    Great Solution is here
    https://discuss.leetcode.com/topic/30508/easiest-java-solution-with-grap
    h-explanation

---
+++ TDDArg.Test/MultiplyStringsTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class MultiplyStringsTest
+    {
+        [Test]
+        public void Given_two_number_one_is_zero_When_multify_Then_zero()
+        {
+            Assert.AreEqual("0", MultiplyStrings.Multiply("123", "0"));
+        }
+
+        [Test]
+        public void Given_two_number_one_is_one_When_multify_Then_other_one()
+        {
+            Assert.AreEqual("123", MultiplyStrings.Multiply("123", "1"));
+        }
+
+        [Test]
+        public void Given_two_number_3_5_When_multify_Then_return_15()
+        {
+            Assert.AreEqual("15", MultiplyStrings.Multiply("5", "3"));
+        }
+
+        [Test]
+        public void Given_two_number_3_10_When_multify_Then_return_30()
+        {
+            Assert.AreEqual("30", MultiplyStrings.Multiply("3", "10"));
+        }
+
+        [Test]
+        public void Given_two_number_When_multify_Then_return()
+        {
+            Assert.AreEqual("15129", MultiplyStrings.Multiply("123", "123"));
+        }
+    }
+}
---
+++ TDDArg/MultiplyStrings.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace TDDArg
+{
+    public class MultiplyStrings
+    {
+        public static string Multiply(string num1, string num2)
+        {
+            var l1 = num1.Length;
+            var l2 = num2.Length;
+
+            int[] array = new int[l1 + l2];
+
+            for (int i = l1 - 1; i >= 0; i--)
+            {
+                for (int j = l2 - 1; j >= 0; j--)
+                {
+                    int c1 = num1[i] - '0';
+                    int c2 = num2[j] - '0';
+
+                    var result = c1 * c2;
+
+                    array[i + j + 1] += result % 10;
+                    if (array[i + j + 1] >= 10)
+                    {
+                        array[i + j]++;
+                        array[i + j + 1] = array[i + j + 1] % 10;
+                    }
+
+                    array[i + j] += result / 10;
+                    if (array[i + j] >= 10)
+                    {
+                        array[i + j -1]++;
+                        array[i + j] = array[i + j] % 10;
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < array.Length; i++)
+            {
+                sb.Append(array[i]);
+            }
+
+            var str = sb.ToString().TrimStart('0');
+            return string.IsNullOrEmpty(str) ? "0" : str;
+        }
+    }
+}
