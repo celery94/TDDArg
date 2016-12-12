9ec497b2595ece203ef51a1ee431bd67e98e7753
Celery Liu <liuct94@gmail.com>
2016-12-12 20:37:35 +0800

    Group Anagrams with performance issue

---
+++ TDDArg.Test/GroupAnagramsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class GroupAnagramsTest
+	{
+		[Test]
+		public void Given_array_When_group_Then_return_one()
+		{
+			var strs = new string[] { "eat", "ate", "tea" };
+
+			var result = GroupAnagrams.FindGroupAnagrams(strs);
+
+			Assert.AreEqual(1, result.Count);
+		}
+
+		[Test]
+		public void Given_array_When_group_Then_return_two()
+		{
+			var strs = new string[] { "eat", "ate", "tea", "aaa" };
+
+			var result = GroupAnagrams.FindGroupAnagrams(strs);
+
+			Assert.AreEqual(2, result.Count);
+		}
+
+
+		[Test]
+		public void Given_array_When_group_Then_return_only_one()
+		{
+			var strs = new string[] { "dad", "day" };
+
+			var result = GroupAnagrams.FindGroupAnagrams(strs);
+
+			Assert.AreEqual(2, result.Count);
+		}
+	}
+}
---
+++ TDDArg/GroupAnagrams.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDDArg
+{
+	public class GroupAnagrams
+	{
+		public static IList<IList<string>> FindGroupAnagrams(string[] strs)
+		{
+			var result = new List<IList<string>>();
+
+			foreach (var str in strs)
+			{
+				bool isFind = false;
+				foreach (var item in result)
+				{
+					if (IsSame(str, item[0]))
+					{
+						item.Add(str);
+						isFind = true;
+						break;
+					}
+				}
+
+				if (!isFind)
+					result.Add(new List<string> { str });
+			}
+
+			return result;
+		}
+
+		public static bool IsSame(string a, string b)
+		{
+			if (a.Length != b.Length) return false;
+
+			var contain = new bool[a.Length];
+
+			foreach (var aItem in a)
+			{
+				for (int i = 0; i < b.Length; i++)
+				{
+					if (contain[i]) continue;
+
+					if (aItem == b[i])
+					{
+						contain[i] = true;
+						break;
+					}
+				}
+			}
+
+			for (int i = 0; i < b.Length; i++)
+			{
+				if (contain[i] == false) return false;
+			}
+
+			return true;
+		}
+	}
+}
