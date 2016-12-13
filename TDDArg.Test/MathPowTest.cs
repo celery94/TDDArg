db247334d3d12783a681f118557d46bb84464846
Celery Liu <liuct94@gmail.com>
2016-12-13 10:54:38 +0800

    MathPow with issue

---
+++ TDDArg.Test/MathPowTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class MathPowTest
+	{
+
+		[Test]
+		public void Given_2_When_pow_0_Then_1()
+		{
+			Assert.AreEqual(1, MathPow.MyPow(2, 0));
+		}
+
+		[Test]
+		public void Given_2_When_pow_2_Then_4()
+		{
+			Assert.AreEqual(4, MathPow.MyPow(2, 2));
+		}
+
+		[Test]
+		[Ignore("failed with double")]
+		public void Given_888023_When_pow_3_Then_70028148()
+		{
+			Assert.AreEqual(700.28148d, MathPow.MyPow(8.88023d, 3));
+		}
+	}
+}
---
+++ TDDArg/MathPow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TDDArg
+{
+	public class MathPow
+	{
+		public static double MyPow(double x, int n)
+		{
+			if (n == 0) return 1;
+
+			return x * MyPow(x, n - 1);
+		}
+	}
+}
