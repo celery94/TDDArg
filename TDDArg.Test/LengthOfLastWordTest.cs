a405c6fff3cad22b477634dcbe73d48ecfa65761
Celery Liu <liuct94@gmail.com>
2016-12-15 21:42:42 +0800

    58. Length of Last Word passed

---
+++ TDDArg.Test/LengthOfLastWordTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class LengthOfLastWordTest
+	{
+		[Test]
+		public void Given_str_When_find_Then_return()
+		{
+			Assert.AreEqual(5, LengthOfLastWord.FindLengthOfLastWord("Hello World"));
+		}
+
+		[Test]
+		public void Given_str_end_with_space_When_find_Then_return_0()
+		{
+			Assert.AreEqual(5, LengthOfLastWord.FindLengthOfLastWord("Hello World "));
+		}
+
+		[Test]
+		public void Given_str_end_without_space_When_find_Then_return_str_length()
+		{
+			Assert.AreEqual(1, LengthOfLastWord.FindLengthOfLastWord("a"));
+		}
+	}
+}
---
+++ TDDArg/LengthOfLastWord.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace TDDArg
+{
+	public class LengthOfLastWord
+	{
+		public static int FindLengthOfLastWord(string s)
+		{
+			int result = 0;
+			s = s.TrimEnd();
+			if (s.LastIndexOf(' ') != -1)
+			{
+				result = s.Substring(s.LastIndexOf(' ') + 1).Length;
+			}
+			else
+			{
+				result = s.Length;
+			}
+
+			return result;
+		}
+	}
+}
