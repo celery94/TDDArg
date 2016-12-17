25db74052e2e5e2764552d8c1004f51f479b17e8
Celery Liu <liuct94@gmail.com>
2016-12-17 09:15:17 +0800

    Maximum Subarray with performance issue

---
+++ TDDArg.Test/MaximumSubarrayTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class MaximumSubarrayTest
+	{
+		[Test]
+		public void Given_array_When_find_Then_return()
+		{
+			var nums = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4 };
+
+			Assert.AreEqual(6, MaximumSubarray.MaxSubArray(nums));
+		}
+	}
+}
---
+++ TDDArg/MaximumSubarray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace TDDArg
+{
+	public class MaximumSubarray
+	{
+		public static int MaxSubArray(int[] nums)
+		{
+			int maxValue = int.MinValue;
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				for (int j = i; j < nums.Length; j++)
+				{
+					var sum = 0;
+					for (int k = i; k <= j; k++)
+					{
+						sum += nums[k];
+					}
+
+					maxValue = Math.Max(maxValue, sum);
+				}
+			}
+
+			return maxValue;
+		}
+	}
+}
