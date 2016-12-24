0a37093016e07e77a24b75561d15be63b5b7d5ab
Celery Liu <liuct94@gmail.com>
2016-12-24 09:31:16 +0800

    55. Jump Game with performance issue

---
+++ TDDArg.Test/JumpGameTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class JumpGameTest
+	{
+		[Test]
+		public void Given_array_When_find_Then_return()
+		{
+			var nums = new int[] { 3, 2, 1, 4 };
+
+			var ret = JumpGame.CanJump(nums);
+
+			Assert.IsTrue(ret);
+		}
+
+		[Test]
+		public void Given_array_When_find_Then_return_false()
+		{
+			var nums = new int[] { 3, 2, 1, 0, 4 };
+
+			var ret = JumpGame.CanJump(nums);
+
+			Assert.IsFalse(ret);
+		}
+	}
+}
---
+++ TDDArg/JumpGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace TDDArg
+{
+	public class JumpGame
+	{
+		public static bool CanJump(int[] nums)
+		{
+			return Jump(0, nums);
+		}
+
+		public static bool Jump(int index, int[] nums)
+		{
+			if (index == nums.Length - 1) return true;
+
+			var current = nums[index];
+
+			if (current == 0) return false;
+
+			for (int i = 1; i <= current; i++)
+			{
+				if (Jump(index + i, nums)) return true;
+			}
+
+			return false;
+		}
+	}
+}
