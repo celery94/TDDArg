49f47b45923514f11c52150f746e2f436e9157b9
Celery Liu <liuct94@gmail.com>
2016-12-28 21:18:24 +0800

    rotate list with issue that k can max than list length

---
+++ TDDArg/RotateList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace TDDArg
+{
+	public class RotateList
+	{
+		public static ListNode RotateRight(ListNode head, int k)
+		{
+			var end = head;
+
+			while (end.next != null)
+			{
+				end = end.next;
+			}
+
+			end.next = head;
+
+			for (int i = 0; i <= k; i++)
+			{
+				head = head.next;
+			}
+
+			var newHead = head.next;
+			head.next = null;
+
+			return newHead;
+		}
+	}
+}
