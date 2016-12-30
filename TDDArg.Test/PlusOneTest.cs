242e2e32ba769cb450cb59b9c204d1002b9602eb
Celery Liu <celery.liu@commscope.com>
2016-12-30 13:38:30 +0800

    66. Plus One passed

---
+++ TDDArg.Test/PlusOneTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class PlusOneTest
+    {
+        [Test]
+        public void Given_1_When_add_Then_return_2()
+        {
+            var digits = new int[] { 1 };
+
+            var result = new int[] { 2 };
+
+            Assert.AreEqual(result, PlusOne.GetPlusOne(digits));
+        }
+
+        [Test]
+        public void Given_9_When_add_Then_return_10()
+        {
+            var digits = new int[] { 9 };
+
+            var result = new int[] { 1, 0 };
+
+            Assert.AreEqual(result, PlusOne.GetPlusOne(digits));
+        }
+    }
+}
---
+++ TDDArg/PlusOne.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace TDDArg
+{
+    public class PlusOne
+    {
+        public static int[] GetPlusOne(int[] digits)
+        {
+            bool plusOne = true;
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (plusOne)
+                {
+                    if (digits[i] + 1 == 10)
+                    {
+                        digits[i] = 0;
+                    }
+                    else
+                    {
+                        digits[i]++;
+                        plusOne = false;
+                        break;
+                    }
+                }
+            }
+
+            if (plusOne)
+            {
+                return new[] { 1 }.Concat(digits).ToArray();
+            }
+
+            return digits;
+        }
+    }
+}
