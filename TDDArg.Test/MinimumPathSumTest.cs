3979a2f4e32018a8e2352d1f651e5f6e1966a908
Celery Liu <celery.liu@commscope.com>
2017-01-03 13:48:35 +0800

    64. Minimum Path Sum with performance issue

---
+++ TDDArg.Test/MinimumPathSumTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Configuration;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class MinimumPathSumTest
+    {
+        [Test]
+        public void Given_grid_When_find_min_Then_return()
+        {
+            var grid = new int[2, 3];
+
+            grid[0, 0] = 1;
+            grid[0, 1] = 2;
+            grid[0, 2] = 3;
+            grid[1, 0] = 4;
+            grid[1, 1] = 5;
+            grid[1, 2] = 6;
+
+            Assert.AreEqual(12, MinimumPathSum.MinPathSum(grid));
+        }
+
+        [Test]
+        public void Given_grid_1_1_When_find_min_Then_return()
+        {
+            var grid = new int[1, 1];
+
+            grid[0, 0] = 1;
+
+            Assert.AreEqual(1, MinimumPathSum.MinPathSum(grid));
+        }
+    }
+}
---
+++ TDDArg/MinimumPathSum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TDDArg
+{
+    public class MinimumPathSum
+    {
+        public static int MinPathSum(int[,] grid)
+        {
+            int minSum = Int32.MaxValue;
+            FindMin(grid, 0, 0, 0, ref minSum);
+
+            return minSum;
+        }
+
+        private static void FindMin(int[,] grid, int x, int y, int sum, ref int minSum)
+        {
+            sum += grid[x, y];
+
+            if (sum > minSum) return;
+
+            var xLength = grid.GetLength(0);
+            var yLength = grid.GetLength(1);
+
+            if (x == xLength - 1 && y == yLength - 1)
+            {
+                minSum = Math.Min(sum, minSum);
+                return;
+            }
+            else if (x == xLength - 1)
+            {
+                FindMin(grid, x, y + 1, sum, ref minSum);
+                return;
+            }
+            else if (y == yLength - 1)
+            {
+                FindMin(grid, x + 1, y, sum, ref minSum);
+                return;
+            }
+
+            FindMin(grid, x + 1, y, sum, ref minSum);
+
+            FindMin(grid, x, y + 1, sum, ref minSum);
+        }
+    }
+}
