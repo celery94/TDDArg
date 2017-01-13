518906b65285b72900ae35e8439d8a621133ef42
Celery Liu <celery.liu@commscope.com>
2017-01-13 15:01:28 +0800

    70. Climbing Stairs with performance issue

---
+++ TDDArg.Test/ClimbingStairsTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class ClimbingStairsTest
+    {
+        [Test]
+        public void Given_1_When_count_Then_return_1()
+        {
+            Assert.AreEqual(1, ClimbingStairs.ClimbStairs(1));
+        }
+
+        [Test]
+        public void Given_2_When_count_Then_return_2()
+        {
+            Assert.AreEqual(2, ClimbingStairs.ClimbStairs(2));
+        }
+
+        [Test]
+        public void Given_3_When_count_Then_return_3()
+        {
+            Assert.AreEqual(3, ClimbingStairs.ClimbStairs(3));
+        }
+    }
+}
---
+++ TDDArg/ClimbingStairs.cs
@@ -0,0 +1,26 @@
+﻿namespace TDDArg
+{
+    public class ClimbingStairs
+    {
+        public static int ClimbStairs(int n)
+        {
+            int count = 0;
+            Climb(n, 0, ref count);
+
+            return count;
+        }
+
+        public static void Climb(int n, int step, ref int count)
+        {
+            n -= step;
+
+            if (n == 0) count++;
+
+            if (n < 0) return;
+
+            Climb(n, 1, ref count);
+
+            Climb(n, 2, ref count);
+        }
+    }
+}
