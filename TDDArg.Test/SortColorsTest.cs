bb6f0b7c0794a687080c37b5568f3c6d0d62232f
Celery Liu <celery.liu@commscope.com>
2017-01-18 13:18:28 +0800

    75. Sort Colors passed

---
+++ TDDArg.Test/SortColorsTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class SortColorsTest
+    {
+        [Test]
+        public void Given_array_When_sort_Then_return()
+        {
+            var array = new[] { 0, 1, 2, 0, 1, 2, 0, 1, 2 };
+
+            SortColors.SortColorsFunc(array);
+
+            Assert.AreEqual(new[] { 0, 0, 0, 1, 1, 1, 2, 2, 2 }, array);
+        }
+    }
+}
---
+++ TDDArg/SortColors.cs
@@ -0,0 +1,35 @@
+﻿namespace TDDArg
+{
+    public class SortColors
+    {
+        public static void SortColorsFunc(int[] nums)
+        {
+            int start = 0;
+            int end = nums.Length - 1;
+
+            int mid = 0;
+            while (mid <= end)
+            {
+                if (nums[mid] == 0)
+                {
+                    var temp = nums[start];
+                    nums[start] = nums[mid];
+                    nums[mid] = temp;
+                    start++;
+                    mid++;
+                }
+                else if (nums[mid] == 2)
+                {
+                    var temp = nums[end];
+                    nums[end] = nums[mid];
+                    nums[mid] = temp;
+                    end--;
+                }
+                else
+                {
+                    mid++;
+                }
+            }
+        }
+    }
+}
