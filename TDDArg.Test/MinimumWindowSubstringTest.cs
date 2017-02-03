be31acd723ca997281f1b94caf9ad632e01303e6
Celery Liu <celery.liu@commscope.com>
2017-02-03 13:31:58 +0800

    76. Minimum Window Substring with issue
    target is duplicate char

---
+++ TDDArg.Test/MinimumWindowSubstringTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class MinimumWindowSubstringTest
+    {
+        [Test]
+        public void Given_str_When_find_Then_return_min()
+        {
+            var str = "ADOBECODEBANC";
+            var target = "ABC";
+
+            Assert.AreEqual("BANC", MinimumWindowSubstring.MinWindow(str, target));
+        }
+
+        [Test]
+        public void Given_a_When_find_aa_Then_return_min()
+        {
+            var str = "a";
+            var target = "aa";
+
+            Assert.AreEqual("a", MinimumWindowSubstring.MinWindow(str, target));
+        }
+    }
+}
---
+++ TDDArg/MinimumWindowSubstring.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TDDArg
+{
+    public class MinimumWindowSubstring
+    {
+        public static string MinWindow(string s, string t)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            foreach (var c in t)
+            {
+                if (!dic.ContainsKey(c))
+                    dic.Add(c, -1);
+            }
+
+            int cursor = 0;
+            var result = string.Empty;
+            while (cursor < s.Length)
+            {
+                if (dic.ContainsKey(s[cursor]))
+                {
+                    dic[s[cursor]] = cursor;
+                }
+
+                if (dic.All(q => q.Value >= 0))
+                {
+                    var start = dic.Min(q => q.Value);
+                    var substr = s.Substring(start, cursor - start + 1);
+
+                    if (result == string.Empty || substr.Length < result.Length)
+                        result = substr;
+                }
+
+                cursor++;
+            }
+
+            return result;
+        }
+    }
+}
