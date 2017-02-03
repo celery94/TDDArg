e1ad8572715106e8c994db721d572994e4d5637b
Celery Liu <celery.liu@commscope.com>
2017-02-03 15:12:04 +0800

    83. Remove Duplicates from Sorted List passed

---
+++ TDDArg.Test/RemoveDuplicatesFromSortedListTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class RemoveDuplicatesFromSortedListTest
+    {
+        [Test]
+        public void Given_list_When_remove_duplicate_Then_return()
+        {
+            ListNode list = new ListNode(1)
+            {
+                next = new ListNode(1)
+                {
+                    next = new ListNode(2)
+                }
+            };
+
+            var result = RemoveDuplicatesFromSortedList.DeleteDuplicates(list);
+
+            Assert.AreEqual(1, result.val);
+            Assert.AreEqual(2, result.next.val);
+        }
+    }
+}
---
+++ TDDArg/RemoveDuplicatesFromSortedList.cs
@@ -0,0 +1,29 @@
+﻿namespace TDDArg
+{
+    public class RemoveDuplicatesFromSortedList
+    {
+        public static ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null) return head;
+
+            var pre = head;
+            var next = head.next;
+
+            while (next != null)
+            {
+                if (next.val == pre.val)
+                {
+                    next = next.next;
+                    pre.next = next;
+                }
+                else
+                {
+                    pre = next;
+                    next = next.next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
