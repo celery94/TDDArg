0986dfccde0f833eaaa0d7f62ac0bf8dfee43cd8
Celery Liu <celery.liu@commscope.com>
2017-02-03 14:31:47 +0800

    80. Remove Duplicates from Sorted Array II passed

---
+++ TDDArg.Test/RemoveDuplicatesFromSortedArrayIITest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class RemoveDuplicatesFromSortedArrayIITest
+    {
+        [Test]
+        public void Given_array_When_remove_Then_return_length()
+        {
+            var nums = new int[] { 1, 1, 1, 2, 2, 3 };
+
+            Assert.AreEqual(5, RemoveDuplicatesFromSortedArrayII.RemoveDuplicates(nums));
+            Assert.AreEqual(1, nums[0]);
+            Assert.AreEqual(1, nums[1]);
+            Assert.AreEqual(2, nums[2]);
+            Assert.AreEqual(2, nums[3]);
+            Assert.AreEqual(3, nums[4]);
+        }
+    }
+}
---
+++ TDDArg/RemoveDuplicatesFromSortedArrayII.cs
@@ -0,0 +1,37 @@
+﻿namespace TDDArg
+{
+    public class RemoveDuplicatesFromSortedArrayII
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            bool isDuplicate = false;
+
+            var count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i == 0)
+                {
+                    count++;
+                }
+                else
+                {
+                    if (nums[i - 1] == nums[i])
+                    {
+                        if (isDuplicate == false)
+                        {
+                            nums[count++] = nums[i];
+                            isDuplicate = true;
+                        }
+                    }
+                    else
+                    {
+                        isDuplicate = false;
+                        nums[count++] = nums[i];
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
