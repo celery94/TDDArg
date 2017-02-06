6c9521409655434ce050831e4339e8530d1736d6
Celery Liu <celery.liu@commscope.com>
2017-02-06 09:53:16 +0800

    82. Remove Duplicates from Sorted List II with issue

---
+++ TDDArg.Test/RemoveDuplicatesFromSortedListIITest.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class RemoveDuplicatesFromSortedListIITest
+    {
+        [Test]
+        public void Given_list_When_remove_Then_return()
+        {
+            ListNode list = new ListNode(1)
+            {
+                next = new ListNode(1)
+                {
+                    next = new ListNode(2)
+                }
+            };
+
+            var result = RemoveDuplicatesFromSortedListII.DeleteDuplicates(list);
+
+            Assert.AreEqual(2, result.val);
+        }
+
+        [Test]
+        public void Given_list_head_duplicate_When_remove_Then_return()
+        {
+            ListNode list = new ListNode(1)
+            {
+                next = new ListNode(1)
+                {
+                    next = new ListNode(2)
+                    {
+                        next = new ListNode(2)
+                        {
+                            next = new ListNode(3)
+                            {
+                                next = new ListNode(4)
+                            }
+                        }
+                    }
+                }
+            };
+
+            var result = RemoveDuplicatesFromSortedListII.DeleteDuplicates(list);
+
+            Assert.AreEqual(3, result.val);
+            Assert.AreEqual(4, result.next.val);
+        }
+
+        //[Test]
+        //public void Given_list_duplicate_When_remove_Then_return()
+        //{
+        //    ListNode list = new ListNode(1)
+        //    {
+        //        next = new ListNode(2)
+        //        {
+        //            next = new ListNode(2)
+        //            {
+        //                next = new ListNode(3)
+        //                {
+        //                    next = new ListNode(4)
+        //                    {
+        //                        next = new ListNode(5)
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    };
+
+        //    var result = RemoveDuplicatesFromSortedListII.DeleteDuplicates(list);
+
+        //    Assert.AreEqual(1, result.val);
+        //    Assert.AreEqual(3, result.next.val);
+        //}
+    }
+}
---
+++ TDDArg/RemoveDuplicatesFromSortedListII.cs
@@ -0,0 +1,52 @@
+﻿namespace TDDArg
+{
+    public class RemoveDuplicatesFromSortedListII
+    {
+        public static ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head?.next == null) return head;
+
+            var next = head.next;
+
+            var preSame = false;
+            while (next != null)
+            {
+                if (head.val == next.val)
+                {
+                    preSame = true;
+                }
+                else
+                {
+                    if (preSame == false)
+                        break;
+
+                    preSame = false;
+                }
+
+                head = next;
+                next = next.next;
+            }
+
+            //if (next != null)
+            //{
+            //    var pre = head;
+            //    var current = next;
+            //    next = next.next;
+            //    while (next != null && current != null)
+            //    {
+            //        if (current.val == next.val)
+            //        {
+            //            current = next;
+            //            next = next.next;
+            //        }
+            //        else
+            //        {
+            //            pre.next = next;
+            //        }
+            //    }
+            //}
+
+            return head;
+        }
+    }
+}
