7a90a587f0fce71dc1929299ba6817c82369b740
Celery Liu <liuct94@gmail.com>
2017-02-11 15:04:27 +0800

    144. Binary Tree Preorder Traversal passed

---
+++ TDDArg.Test/BinaryTreePreorderTraversalTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class BinaryTreePreorderTraversalTest
+	{
+		[Test]
+		public void Given_tree_When_traversal_return()
+		{
+			TreeNode node = new TreeNode(1)
+			{
+				left = new TreeNode(2),
+				right = new TreeNode(3)
+			};
+
+			var result = BinaryTreePreorderTraversal.PreorderTraversal(node);
+
+			Assert.AreEqual(1, result[0]);
+			Assert.AreEqual(2, result[1]);
+			Assert.AreEqual(3, result[2]);
+		}
+	}
+}
---
+++ TDDArg/BinaryTreePreorderTraversal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDDArg
+{
+	public class BinaryTreePreorderTraversal
+	{
+		public static IList<int> PreorderTraversal(TreeNode root)
+		{
+			List<int> list = new List<int>();
+
+			if (root != null)
+			{
+				list.Add(root.val);
+				list.AddRange(PreorderTraversal(root.left));
+				list.AddRange(PreorderTraversal(root.right));
+			}
+
+			return list;
+		}
+	}
+}
