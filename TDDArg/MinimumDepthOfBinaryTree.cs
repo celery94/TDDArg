75083bb0ba106522d80d9b7b9524a89c5dcccfd7
Celery Liu <liuct94@gmail.com>
2017-02-11 13:30:41 +0800

    111. Minimum Depth of Binary Tree passed

---
+++ TDDArg/MinimumDepthOfBinaryTree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace TDDArg
+{
+	public class MinimumDepthOfBinaryTree
+	{
+		public int MinDepth(TreeNode root)
+		{
+			if (root == null) return 0;
+
+			if (root.left == null) return 1 + Min(root.right);
+			if (root.right == null) return 1 + Min(root.right);
+
+			return 1 + Math.Min(Min(root.left), Min(root.right));
+		}
+
+		private int Min(TreeNode root) { 
+			if (root == null) return 0;
+
+			return 1 + Math.Min(MinDepth(root.left), MinDepth(root.right));
+		}
+	}
+}
