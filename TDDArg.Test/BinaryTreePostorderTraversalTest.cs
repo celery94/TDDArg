4b3c775c5b86beee1d610b3fe93c88746ba9e7e4
Celery Liu <liuct94@gmail.com>
2017-02-11 15:57:17 +0800

    145. Binary Tree Postorder Traversal recursive passed

---
+++ TDDArg.Test/BinaryTreePostorderTraversalTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class BinaryTreePostorderTraversalTest
+	{
+		[Test]
+		public void Given_tree_When_traversal_return()
+		{
+			TreeNode node = new TreeNode(1)
+			{
+				left = new TreeNode(2),
+				right = new TreeNode(3)
+			};
+
+			var result = BinaryTreePostorderTraversal.PostorderTraversal(node);
+
+			Assert.AreEqual(2, result[0]);
+			Assert.AreEqual(3, result[1]);
+			Assert.AreEqual(1, result[2]);
+		}
+	}
+}
---
+++ TDDArg/BinaryTreePostorderTraversal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDDArg
+{
+	public class BinaryTreePostorderTraversal
+	{
+		public static IList<int> PostorderTraversal(TreeNode root)
+		{
+			List<int> list = new List<int>();
+			Traversal(root, list);
+			return list;
+		}
+
+		private static void Traversal(TreeNode node, List<int> list)
+		{
+			if (node == null) return;
+			Traversal(node.left, list);
+			Traversal(node.right, list);
+			list.Add(node.val);
+		}
+	}
+}
