af95a8bb4534f1301b743d53ffe200b7ac19d43d
Celery Liu <liuct94@gmail.com>
2017-02-11 14:50:52 +0800

    108. Convert Sorted Array to Binary Search Tree passed

---
+++ TDDArg.Test/ConvertSortedArrayToBinarySearchTreeTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class ConvertSortedArrayToBinarySearchTreeTest
+	{
+		[Test]
+		public void Given_empty_array_When_convert_Then_return_null()
+		{
+			Assert.AreEqual(null, ConvertSortedArrayToBinarySearchTree.SortedArrayToBST(new int[] { }));
+		}
+
+		[Test]
+		public void Given_array_1_item_When_convert_Then_return_root()
+		{
+			var result = ConvertSortedArrayToBinarySearchTree.SortedArrayToBST(new int[] { 1 });
+			Assert.AreEqual(1, result.val);
+		}
+
+		[Test]
+		public void Given_array_2_item_When_convert_Then_return_root()
+		{
+			var result = ConvertSortedArrayToBinarySearchTree.SortedArrayToBST(new int[] { 1, 2 });
+			Assert.AreEqual(1, result.val);
+		}
+	}
+}
---
+++ TDDArg/ConvertSortedArrayToBinarySearchTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace TDDArg
+{
+	public class ConvertSortedArrayToBinarySearchTree
+	{
+		public static TreeNode SortedArrayToBST(int[] nums)
+		{
+			if (nums.Length == 0) return null;
+			return Append(nums, 0, nums.Length - 1);
+		}
+
+		private static TreeNode Append(int[] nums, int s, int e)
+		{
+			if (s > e) return null;
+
+			var mid = (s + e) / 2;
+
+			var node = new TreeNode(nums[mid]);
+			node.left = Append(nums, 0, mid - 1);
+			node.right = Append(nums, mid + 1, e);
+
+			return node;
+		}
+	}
+}
