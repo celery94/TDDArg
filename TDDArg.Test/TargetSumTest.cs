ffdc654c44e49cc35429174a002074c3df74a147
Celery Liu <liuct94@gmail.com>
2017-02-12 15:54:31 +0800

    494. Target Sum passed

---
+++ TDDArg.Test/TargetSumTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class TargetSumTest
+	{
+		[Test]
+		public void Given_array_When_find_Then_return()
+		{
+			var nums = new int[] { 1, 1, 1, 1, 1 };
+
+			Assert.AreEqual(5, TargetSum.FindTargetSumWays(nums, 3));
+		}
+	}
+}
---
+++ TDDArg/TargetSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace TDDArg
+{
+	public class TargetSum
+	{
+		public static int FindTargetSumWays(int[] nums, int S)
+		{
+			int result = 0;
+			Helper(nums, 0, S, ref result);
+
+			return result;
+		}
+
+		private static void Helper(int[] nums, int index, int target, ref int result)
+		{
+			if (target == 0 && index == nums.Length)
+				result++;
+			if (index >= nums.Length) return;
+
+			Helper(nums, index + 1, target - nums[index], ref result);
+			Helper(nums, index + 1, target + nums[index], ref result);
+		}
+	}
+}
