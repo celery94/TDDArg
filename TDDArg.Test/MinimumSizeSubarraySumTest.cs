a9b2cb25015544139f024134949447bb6a2e0c53
Celery Liu <liuct94@gmail.com>
2017-02-12 16:27:12 +0800

    209. Minimum Size Subarray Sum passed

---
+++ TDDArg.Test/MinimumSizeSubarraySumTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class MinimumSizeSubarraySumTest
+	{
+		[Test]
+		public void Given_nums_When_find_Then_return()
+		{
+			var nums = new int[] { 2, 3, 1, 2, 4, 3 };
+
+			Assert.AreEqual(2, MinimumSizeSubarraySum.MinSubArrayLen(7, nums));
+		}
+	}
+}
---
+++ TDDArg/MinimumSizeSubarraySum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace TDDArg
+{
+	public class MinimumSizeSubarraySum
+	{
+		public static int MinSubArrayLen(int s, int[] nums)
+		{
+			if (nums.Length == 0) return 0;
+
+			int start = 0;
+			int end = 1;
+
+			int length = 0;
+			int sum = nums[start];
+
+			if (sum >= s) return 1;
+
+			while (end < nums.Length)
+			{
+				sum += nums[end];
+
+				if (sum >= s)
+				{
+					while (sum - nums[start] >= s)
+					{
+						sum -= nums[start++];
+					}
+
+					if (length == 0 || end - start + 1 < length)
+					{
+						length = end - start + 1;
+
+					}
+				}
+
+				end++;
+			}
+
+			return length;
+		}
+	}
+}
