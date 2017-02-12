d66cb3fde6252731c183a12d1a96dbc4d3493f7b
Celery Liu <liuct94@gmail.com>
2017-02-12 15:02:48 +0800

    PartitionEqualSubsetSum with performance issue

---
+++ TDDArg.Test/PartitionEqualSubsetSumTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class PartitionEqualSubsetSumTest
+	{
+		[Test]
+		public void Given_array_When_find_Then_return()
+		{
+			Assert.AreEqual(true, PartitionEqualSubsetSum.CanPartition(new int[] { 1, 11, 5, 5 }));
+		}
+	}
+}
---
+++ TDDArg/PartitionEqualSubsetSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace TDDArg
+{
+	public class PartitionEqualSubsetSum
+	{
+		public static bool CanPartition(int[] nums)
+		{
+			var sum = nums.ToList().Sum();
+			if (sum % 2 == 1) return false;
+
+			return Helper(nums.OrderBy(q => q).ToArray(), 0, sum / 2);
+		}
+
+		private static bool Helper(int[] nums, int index, int target)
+		{
+			if (target == 0) return true;
+			if (target < 0) return false;
+
+			for (int i = index; i < nums.Length; i++)
+			{
+				if (Helper(nums, i + 1, target - nums[i])) return true;
+			}
+
+			return false;
+		}
+	}
+}
