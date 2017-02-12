6b95840c206bd887e589bf5c18fe616dcff34bcc
Celery Liu <liuct94@gmail.com>
2017-02-12 17:46:28 +0800

    344. Reverse String passed

---
+++ TDDArg/ReverseString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace TDDArg
+{
+	public class ReverseString
+	{
+		public string ReverseStringHelper(string s)
+		{
+			var array = s.ToCharArray();
+
+			int start = 0;
+			int end = array.Length - 1;
+
+			while (start < end)
+			{
+				var tmp = array[start];
+				array[start] = array[end];
+				array[end] = tmp;
+
+				start++;
+				end--;
+			}
+
+			return string.Join("", array);
+		}
+	}
+}
