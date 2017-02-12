b06b9d8354caaa43244d50c65e3e6c1a222a05a2
Celery Liu <liuct94@gmail.com>
2017-02-12 13:12:19 +0800

    path sum II with issue

---
+++ TDDArg/PathSumII.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDDArg
+{
+	public class PathSumII
+	{
+		public static IList<IList<int>> PathSum(TreeNode root, int sum)
+		{
+			var result = new List<IList<int>>();
+			var currentPath = new List<int>();
+			FindPathSum(root, sum, result, currentPath);
+
+			return result;
+		}
+
+		private static void FindPathSum(TreeNode node, int sum, List<IList<int>> list, List<int> currentPath)
+		{
+			if (node == null) return;
+
+			currentPath.Add(node.val);
+
+			if (node.left == null && node.right == null && node.val == sum)
+			{
+				list.Add(currentPath);
+				currentPath.Remove(currentPath.Count - 1);
+				return;
+			}
+			else
+			{
+				FindPathSum(node.left, sum - node.val, list, currentPath);
+				FindPathSum(node.right, sum - node.val, list, currentPath);
+			}
+
+			currentPath.Remove(currentPath.Count - 1);
+		}
+	}
+}
