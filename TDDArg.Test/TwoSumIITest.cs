da817de3bd4c71eaf0a9079d50334451396b15ea
Celery Liu <liuct94@gmail.com>
2017-02-12 13:23:07 +0800

    167. Two Sum II - Input array is sorted passed

---
+++ TDDArg.Test/TwoSumIITest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class TwoSumIITest
+	{
+		[Test]
+		public void Given_array_When_find_sum_Then_return()
+		{
+			var nums = new int[] { 2, 7, 11, 15 };
+			var result = TwoSumII.TwoSum(nums, 9);
+
+			Assert.AreEqual(2, result[0]);
+			Assert.AreEqual(7, result[1]);
+		}
+	}
+}
---
+++ TDDArg/TwoSumII.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace TDDArg
+{
+	public class TwoSumII
+	{
+		public static int[] TwoSum(int[] numbers, int target)
+		{
+			int s = 0;
+			int e = numbers.Length - 1;
+
+			while (s < e)
+			{
+				if ((numbers[s] + numbers[e]) == target)
+					return new int[] { numbers[s], numbers[e] };
+				else if ((numbers[s] + numbers[e]) < target)
+					s++;
+				else
+					e--;
+			}
+
+			return new int[] { };
+		}
+	}
+}
