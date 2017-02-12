c878473a9c04f4c7b0bb1100e344f5c78f36b467
Celery Liu <liuct94@gmail.com>
2017-02-12 14:12:35 +0800

    141. Linked List Cycle passed

---
+++ TDDArg/LinkedListCycle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace TDDArg
+{
+	public class LinkedListCycle
+	{
+		public bool HasCycle(ListNode head)
+		{
+			var next = head;
+			while (head != null && next != null)
+			{
+				head = head.next;
+				next = next.next?.next;
+
+				if (head != null && head == next) return true;
+			}
+
+			return false;
+		}
+	}
+}
