62b4906137ec0f2c7079bc946489f57d224be485
Celery Liu <liuct94@gmail.com>
2017-02-12 15:26:46 +0800

    290. Word Pattern passed

---
+++ TDDArg.Test/WordPatternTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class WordPatternTest
+	{
+		[Test]
+		public void Given_pattern_str_When_match_Then_return()
+		{
+			var pattern = "abba";
+			var str = "dog cat cat dog";
+			Assert.AreEqual(true, WordPattern.FindWordPattern(pattern, str));
+		}
+
+		[Test]
+		public void Given_pattern_str_When_match_Then_return_false()
+		{
+			var pattern = "abbb";
+			var str = "dog cat cat dog";
+			Assert.AreEqual(false, WordPattern.FindWordPattern(pattern, str));
+		}
+
+
+		[Test]
+		public void Given_string_same_When_match_Then_return_false()
+		{
+			var pattern = "abba";
+			var str = "dog dog dog dog";
+			Assert.AreEqual(false, WordPattern.FindWordPattern(pattern, str));
+		}
+	}
+}
---
+++ TDDArg/WordPattern.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDDArg
+{
+	public class WordPattern
+	{
+		public static bool FindWordPattern(string pattern, string str)
+		{
+			var strArray = str.Split(' ');
+
+			if (strArray.Length != pattern.Length) return false;
+
+			Dictionary<string, char> dic = new Dictionary<string, char>();
+			for (int i = 0; i < strArray.Length; i++)
+			{
+				if (dic.ContainsKey(strArray[i]))
+				{
+					if (pattern[i] != dic[strArray[i]]) return false;
+				}
+				else  if(dic.ContainsValue(pattern[i]))
+				{
+					return false;
+				}
+				else
+				{
+					dic.Add(strArray[i],pattern[i]);
+				}
+			}
+
+			return true;
+		}
+	}
+}
