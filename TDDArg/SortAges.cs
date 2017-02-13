45191cfe0b17c6126a3a113d206068002229e392
Celery Liu <liuct94@gmail.com>
2017-02-13 21:44:54 +0800

    SortAges in O(n)

---
+++ TDDArg/SortAges.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace TDDArg
+{
+	public class SortAges
+	{
+		public static void Sort(int[] ages)
+		{
+			var range = new int[200];
+
+			foreach (var age in ages)
+			{
+				range[age]++;
+			}
+
+			int index = 0;
+			for (int i = 0; i < 200; i++)
+			{
+				for (int j = 0; j < range[i]; j++)
+				{
+					ages[index] = i;
+					index++;
+				}
+			}
+		}
+	}
+}
