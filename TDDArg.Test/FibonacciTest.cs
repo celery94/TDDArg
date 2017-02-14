c805ea6bba1dab68efc44e9ebc8771e672e2f204
Celery Liu <liuct94@gmail.com>
2017-02-14 20:53:00 +0800

    Fibonacci pass

---
+++ TDDArg.Test/FibonacciTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class FibonacciTest
+	{
+
+		[Test]
+		public void Given_0_When_Sum_Then_return_0()
+		{
+			Assert.AreEqual(0, Fibonacci.FindSum(0));
+		}
+
+		[Test]
+		public void Given_1_When_Sum_Then_return_1()
+		{
+			Assert.AreEqual(1, Fibonacci.FindSum(1));
+		}
+
+		[Test]
+		public void Given_2_When_Sum_Then_return_2()
+		{
+			Assert.AreEqual(1, Fibonacci.FindSum(2));
+		}
+
+		[Test]
+		public void Given_3_When_Sum_Then_return_2()
+		{
+			Assert.AreEqual(2, Fibonacci.FindSum(3));
+		}
+
+		[Test]
+		public void Given_4_When_Sum_Then_return_3()
+		{
+			Assert.AreEqual(3, Fibonacci.FindSum(4));
+		}
+
+		[Test]
+		public void Given_5_When_Sum_Then_return_5()
+		{
+			Assert.AreEqual(5, Fibonacci.FindSum(5));
+		}
+	}
+}
---
+++ TDDArg/Fibonacci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace TDDArg
+{
+	public class Fibonacci
+	{
+		public static int FindSum(int n)
+		{
+			if (n == 0 || n == 1) return n;
+
+			var result = new int[] { 0, 1 };
+			for (int i = 2; i <= n; i++)
+			{
+				var sum = result[0] + result[1];
+				result[0] = result[1];
+				result[1] = sum;
+			}
+
+			return result[1];
+		}
+	}
+}
