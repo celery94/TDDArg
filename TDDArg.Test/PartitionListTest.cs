93b3e5b40ee4f83b8caacdd4298c34253868288f
Celery Liu <liuct94@gmail.com>
2017-02-14 13:43:20 +0800

    86. Partition List passed

---
+++ TDDArg.Test/PartitionListTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class PartitionListTest
+    {
+        [Test]
+        public void Given_list_When_partition_3_Then_return()
+        {
+            var list = new ListNode(5)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                    },
+                }
+            };
+
+            var result = PartitionList.Partition(list, 3);
+
+            Assert.AreEqual(2, result.val);
+        }
+
+        [Test]
+        public void Given_list_When_partition_2_Then_return()
+        {
+            var list = new ListNode(2)
+            {
+                next = new ListNode(1)
+                {
+                }
+            };
+
+            var result = PartitionList.Partition(list, 2);
+
+            Assert.AreEqual(1, result.val);
+        }
+    }
+}
---
+++ TDDArg/PartitionList.cs
@@ -0,0 +1,53 @@
+﻿namespace TDDArg
+{
+    public class PartitionList
+    {
+        public static ListNode Partition(ListNode head, int x)
+        {
+            if (head == null) return null;
+
+            ListNode less = null;
+            ListNode lessH = null;
+            ListNode bigger = null;
+            ListNode biggerH = null;
+
+            while (head != null)
+            {
+                if (head.val < x)
+                {
+                    if (less == null)
+                    {
+                        lessH = less = head;
+                    }
+                    else
+                    {
+                        less.next = head;
+                        less = less.next;
+                    }
+                }
+                else
+                {
+                    if (bigger == null)
+                    {
+                        biggerH = bigger = head;
+                    }
+                    else
+                    {
+                        bigger.next = head;
+                        bigger = bigger.next;
+                    }
+                }
+                head = head.next;
+            }
+
+            //mark tie as null
+            if (bigger != null)
+                bigger.next = null;
+
+            if (lessH == null) return biggerH;
+
+            less.next = biggerH;
+            return lessH;
+        }
+    }
+}
