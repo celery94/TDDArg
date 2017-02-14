d5fc143786d48f22200568fb34a501007bb32cda
Celery Liu <liuct94@gmail.com>
2017-02-14 15:06:08 +0800

    101. Symmetric Tree passed

---
+++ TDDArg/SymmetricTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDArg
+{
+    public class SymmetricTree
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null) return true;
+
+            return IsSymmetric(root.left, root.right);
+        }
+
+        private bool IsSymmetric(TreeNode left, TreeNode right)
+        {
+            return left == null && right == null ||
+                (left?.val == right?.val && IsSymmetric(left?.left, right?.right) && IsSymmetric(left?.right, right?.left));
+        }
+    }
+}
