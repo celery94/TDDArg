d628e5db957789c91dec3660c0b5d87c0b7d9f94
Celery Liu <celery.liu@commscope.com>
2017-02-14 13:16:17 +0800

    92. Reverse Linked List II passed

---
+++ TDDArg.Test/ReverseLinkedListIITest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class ReverseLinkedListIITest
+    {
+        [Test]
+        public void Given_list_When_reserse_2_4_Then_return()
+        {
+            var root = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                        {
+                            next = new ListNode(5)
+                        }
+                    }
+                }
+            };
+
+            var result = ReverseLinkedListII.ReverseBetween(root, 2, 4);
+
+            Assert.AreEqual(1, result.val);
+            Assert.AreEqual(4, result.next.val);
+        }
+
+        [Test]
+        public void Given_list_When_reserse_3_4_Then_return()
+        {
+            var root = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                        {
+                            next = new ListNode(5)
+                        }
+                    }
+                }
+            };
+
+            var result = ReverseLinkedListII.ReverseBetween(root, 3, 4);
+
+            Assert.AreEqual(1, result.val);
+            Assert.AreEqual(2, result.next.val);
+            Assert.AreEqual(4, result.next.next.val);
+        }
+
+        [Test]
+        public void Given_list_When_reserse_1_1_Then_return()
+        {
+            var root = new ListNode(1)
+            {
+                
+            };
+
+            var result = ReverseLinkedListII.ReverseBetween(root, 1, 1);
+
+            Assert.AreEqual(1, result.val);
+        }
+    }
+}
---
+++ TDDArg/ReverseLinkedListII.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDArg
+{
+    public class ReverseLinkedListII
+    {
+        public static ListNode ReverseBetween(ListNode head, int m, int n)
+        {
+            ListNode pre = null;
+
+            int index = 1;
+            if (m > 1)
+            {
+                pre = head;
+                index++;
+
+                while (index < m)
+                {
+                    pre = pre.next;
+                    index++;
+                }
+            }
+
+            var current = pre == null ? head : pre.next;
+            var next = current.next;
+
+            var mnode = current;
+            var mpre = pre;
+
+            while (m <= n)
+            {
+                current.next = pre;
+                pre = current;
+                current = next;
+                next = next?.next;
+                m++;
+            }
+
+            mnode.next = current;
+
+            if (mpre != null)
+            {
+                mpre.next = pre;
+                return head;
+            }
+            else
+            {
+                return pre;
+            }
+        }
+    }
+}
