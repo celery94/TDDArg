1567a8a7ee3ddf40066551a72e73d94198ac9950
Celery Liu <liuct94@gmail.com>
2017-02-14 14:31:01 +0800

    94. Binary Tree Inorder Traversal passed

---
+++ TDDArg/BinaryTreeInorderTraversal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace TDDArg
+{
+    public class BinaryTreeInorderTraversal
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            if (root == null) return new List<int>();
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            stack.Push(root);
+
+            while (root.left != null)
+            {
+                stack.Push(root.left);
+                root = root.left;
+            }
+
+            List<int> result = new List<int>();
+            while (stack.Count > 0)
+            {
+                var item = stack.Pop();
+                result.Add(item.val);
+
+                if (item.right != null)
+                {
+                    stack.Push(item.right);
+
+                    var left = item.right.left;
+                    while (left != null)
+                    {
+                        stack.Push(left);
+                        left = left.left;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
