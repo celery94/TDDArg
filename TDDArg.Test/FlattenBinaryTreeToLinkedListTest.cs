501eb736402cd4db8f61c73aae90a01905e9f121
Celery Liu <liuct94@gmail.com>
2017-02-15 11:34:34 +0800

    114. Flatten Binary Tree to Linked List passed

---
+++ TDDArg.Test/FlattenBinaryTreeToLinkedListTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class FlattenBinaryTreeToLinkedListTest
+    {
+        [Test]
+        public void Given_tree_When_flatten_Then_return()
+        {
+            var tree = new TreeNode(1)
+            {
+                left = new TreeNode(2)
+            };
+
+
+            FlattenBinaryTreeToLinkedList.Flatten(tree);
+
+            Assert.AreEqual(1, tree.val);
+            Assert.AreEqual(2, tree.right.val);
+
+        }
+
+        [Test]
+        public void Given_tree_3_When_flatten_Then_return()
+        {
+            var tree = new TreeNode(1)
+            {
+                left = new TreeNode(2),
+                right = new TreeNode(3)
+            };
+
+
+            FlattenBinaryTreeToLinkedList.Flatten(tree);
+
+            Assert.AreEqual(1, tree.val);
+            Assert.AreEqual(2, tree.right.val);
+            Assert.AreEqual(3, tree.right.right.val);
+        }
+    }
+}
---
+++ TDDArg/FlattenBinaryTreeToLinkedList.cs
@@ -0,0 +1,41 @@
+﻿namespace TDDArg
+{
+    /// <summary>
+    /// 主要的解决方案还是用一个指针记录平板化后的最后一个节点在哪里
+    /// </summary>
+    public class FlattenBinaryTreeToLinkedList
+    {
+        private static TreeNode pre = null;
+        public static void Flatten(TreeNode root)
+        {
+            if (root == null) return;
+            var left = root.left;
+            var right = root.right;
+
+            if (left != null)
+            {
+                root.right = left;
+                root.left = null;
+                pre = root.right;
+                Flatten(left);
+            }
+
+            if (right != null)
+            {
+                if (pre == null)
+                {
+                    root.right = right;
+                    root.left = null;
+                    pre = root.right;
+                }
+                else
+                {
+                    pre.right = right;
+                    pre.left = null;
+                    pre = pre.right;
+                }
+                Flatten(right);
+            }
+        }
+    }
+}
