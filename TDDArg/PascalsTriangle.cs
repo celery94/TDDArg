98406ab0ea5ecc0034d7d54474d342fd1757093e
Celery Liu <liuct94@gmail.com>
2017-02-15 13:53:54 +0800

    118. Pascal's Triangle passed

---
+++ TDDArg/PascalsTriangle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace TDDArg
+{
+    public class PascalsTriangle
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+
+            List<int> preList = new List<int>();
+            for (int i = 1; i <= numRows; i++)
+            {
+                if (i == 1)
+                {
+                    var list = preList = new List<int>() { 1 };
+                    result.Add(list);
+                }
+                else
+                {
+                    List<int> list = new List<int>() { 1 };
+                    for (int j = 2; j <= i; j++)
+                    {
+                        if (j == i)
+                        {
+                            list.Add(1);
+                        }
+                        else
+                        {
+                            list.Add(preList[j - 1] + preList[j - 2]);
+                        }
+                    }
+                    preList = list;
+                    result.Add(list);
+                }
+            }
+
+            return result;
+        }
+    }
+}
