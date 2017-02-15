8dcc3c0f286f126ff05f82c77b89f31e9336f004
Celery Liu <liuct94@gmail.com>
2017-02-15 14:39:46 +0800

    120. Triangle with performance issue

---
+++ TDDArg.Test/TriangleTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class TriangleTest
+    {
+        [Test]
+        public void Given_triangle_When_find_Then_return()
+        {
+            var triangle = new List<IList<int>>
+            {
+                new List<int> {1},
+                new List<int> {1,2},
+                new List<int> {1, 2,3},
+                new List<int> {1, 2,3,4},
+            };
+
+            Assert.AreEqual(4, new Triangle().MinimumTotal(triangle));
+        }
+    }
+}
---
+++ TDDArg/Triangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDDArg
+{
+    public class Triangle
+    {
+        public int MinimumTotal(IList<IList<int>> triangle)
+        {
+            return FindMin(triangle, 0, 0);
+        }
+
+        private int FindMin(IList<IList<int>> triangle, int level, int index)
+        {
+            if (triangle.Count <= level) return 0;
+
+            var list = triangle[level];
+
+            return list[index] + Math.Min(FindMin(triangle, level + 1, index), FindMin(triangle, level + 1, index + 1));
+        }
+    }
+}
