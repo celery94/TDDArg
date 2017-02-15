8bb941698b4d5157686e6c771a00d9428be554f5
Celery Liu <liuct94@gmail.com>
2017-02-15 16:42:10 +0800

    122. Best Time to Buy and Sell Stock II passed

---
+++ TDDArg/BestTimeToBuyAndSellStockII.cs
@@ -0,0 +1,39 @@
+﻿namespace TDDArg
+{
+    public class BestTimeToBuyAndSellStockII
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Length <= 1) return 0;
+
+            var profit = 0;
+            var buy = -1;
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                if (buy == -1)
+                {
+                    if (prices[i + 1] > prices[i])
+                    {
+                        buy = prices[i];
+                    }
+                }
+                else
+                {
+                    if (prices[i + 1] > prices[i])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        profit += prices[i] - buy;
+                        buy = -1;
+                    }
+                }
+            }
+
+            if (buy != -1) profit += prices[prices.Length - 1] - buy;
+
+            return profit;
+        }
+    }
+}
