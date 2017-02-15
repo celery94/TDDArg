de33eb7963d8f094494d3ef64e4edb3d39b25966
Celery Liu <liuct94@gmail.com>
2017-02-15 17:14:15 +0800

    125. Valid Palindrome passed

---
+++ TDDArg/ValidPalindrome.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TDDArg
+{
+    public class ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return true;
+
+            int start = 0;
+            int end = s.Length - 1;
+
+            while (start < end)
+            {
+                if (!Char.IsLetterOrDigit((s[start]))) { start++; continue; }
+                if (!Char.IsLetterOrDigit((s[end]))) { end--; continue; }
+
+                if (Char.ToLower(s[start]) != Char.ToLower(s[end]))
+                {
+                    return false;
+                }
+                else
+                {
+                    start++;
+                    end--;
+                }
+            }
+
+            return true;
+        }
+    }
+}
