8bf4d1c764970eb07f35c5addd160243a05db81c
Celery Liu <liuct94@gmail.com>
2017-02-15 14:58:05 +0800

    120. Triangle passed

--- TDDArg/Triangle.cs
+++ TDDArg/Triangle.cs
@@ -5,18 +5,22 @@ namespace TDDArg
 {
     public class Triangle
     {
+        /// <summary>
+        /// 可以反过来想，从最低层选小的值开始往上加，记录下来和之后的值，到顶部就是和最小的了
+        /// </summary>
+        /// <param name="triangle"></param>
+        /// <returns></returns>
         public int MinimumTotal(IList<IList<int>> triangle)
         {
-            return FindMin(triangle, 0, 0);
-        }
+            for (int i = triangle.Count - 2; i >= 0; i--)
+            {
+                for (int j = 0; j < triangle[i].Count; j++)
+                {
+                    triangle[i][j] += Math.Min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+                }
+            }
 
-        private int FindMin(IList<IList<int>> triangle, int level, int index)
-        {
-            if (triangle.Count <= level) return 0;
-
-            var list = triangle[level];
-
-            return list[index] + Math.Min(FindMin(triangle, level + 1, index), FindMin(triangle, level + 1, index + 1));
+            return triangle[0][0];
         }
     }
 }
