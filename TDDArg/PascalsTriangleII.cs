5d88913c6104a2e7ad11cb8ab41cddf5fa708d09
Celery Liu <liuct94@gmail.com>
2017-02-15 14:19:59 +0800

    119. Pascal's Triangle II passed

---
+++ TDDArg/PascalsTriangleII.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace TDDArg
+{
+    public class PascalsTriangleII
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i <= rowIndex; i++)
+            {
+                result.Add(1);
+                for (int j = i / 2; j >= 1; j--)
+                {
+                    result[j] = result[j] + result[j - 1];
+                }
+
+                for (int j = i; j > i / 2; j--)
+                {
+                    result[j] = result[i - j];
+                }
+            }
+
+            return result;
+        }
+    }
+}
