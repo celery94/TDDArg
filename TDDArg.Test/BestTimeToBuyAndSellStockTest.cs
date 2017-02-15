d1821742227b3c0fb698941b2986c4370d7cb050
Celery Liu <liuct94@gmail.com>
2017-02-15 15:59:28 +0800

    121. Best Time to Buy and Sell Stock passed

---
+++ TDDArg.Test/BestTimeToBuyAndSellStockTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class BestTimeToBuyAndSellStockTest
+    {
+        [Test]
+        public void Given_prices_When_find_Then_return()
+        {
+            var prices = new int[] { 7, 1, 5, 3, 6, 4 };
+
+            Assert.AreEqual(5, new BestTimeToBuyAndSellStock().MaxProfit(prices));
+        }
+    }
+}
---
+++ TDDArg/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TDDArg
+{
+    public class BestTimeToBuyAndSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Length <= 1) return 0;
+
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                prices[i] = prices[i + 1] - prices[i];
+            }
+
+            var maxProfit = prices[0];
+            var max = maxProfit;
+
+            for (int i = 1; i < prices.Length - 1; i++)
+            {
+                if (prices[i] > maxProfit + prices[i])
+                {
+                    maxProfit = prices[i];
+                }
+                else
+                {
+                    maxProfit = maxProfit + prices[i];
+                }
+
+                max = Math.Max(max, maxProfit);
+            }
+
+            return max > 0 ? max : 0;
+        }
+    }
+}
