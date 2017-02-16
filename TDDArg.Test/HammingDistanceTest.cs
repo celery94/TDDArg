e2964a806f93435c90cfcc28070879b28268265f
Celery Liu <liuct94@gmail.com>
2017-02-16 12:59:18 +0800

    461. Hamming Distance passed

---
+++ TDDArg.Test/HammingDistanceTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+    [TestFixture]
+    public class HammingDistanceTest
+    {
+        [Test]
+        public void Given_x_y_When_find_Then_return()
+        {
+            Assert.AreEqual(2, new HammingDistance().FindHammingDistance(1, 4));
+        }
+    }
+}
---
+++ TDDArg/HammingDistance.cs
@@ -0,0 +1,19 @@
+﻿namespace TDDArg
+{
+    public class HammingDistance
+    {
+        public int FindHammingDistance(int x, int y)
+        {
+            var result = x ^ y;
+
+            int count = 0;
+            while (result > 0)
+            {
+                if (result % 2 == 1) count++;
+                result = result >> 1;
+            }
+
+            return count;
+        }
+    }
+}
