529a732d8466f98bea2c530b63ea3e1694c9da5f
Celery Liu <liuct94@gmail.com>
2017-02-17 12:58:30 +0800

    404. Sum of Left Leaves passed

---
+++ TDDArg/SumOfLeftLeaves.cs
@@ -0,0 +1,22 @@
+﻿namespace TDDArg
+{
+    public class SumOfLeftLeaves
+    {
+        public int FindSumOfLeftLeaves(TreeNode root)
+        {
+            return Sum(root, false);
+        }
+
+
+        public int Sum(TreeNode node, bool isLeft)
+        {
+            if (node == null) return 0;
+
+            if (node.left == null && node.right == null && isLeft) return node.val;
+            if (node.left == null && node.right == null && !isLeft) return 0;
+
+            return Sum(node.left, true) + Sum(node.right, false);
+        }
+
+    }
+}
