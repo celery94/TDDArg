f38bc44045db46bb676f866a5456af721d0d1236
Celery Liu <liuct94@gmail.com>
2017-02-18 10:14:39 +0800

    155. Min Stack passed

---
+++ TDDArg/MinStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDDArg
+{
+	public class MinStack
+	{
+		Stack<int> stack;
+		Stack<int> minStack;
+
+		/** initialize your data structure here. */
+		public MinStack()
+		{
+			stack = new Stack<int>();
+			minStack = new Stack<int>();
+		}
+
+		public void Push(int x)
+		{
+			stack.Push(x);
+
+			if (minStack.Count == 0)
+			{
+				minStack.Push(x);
+			}
+			else {
+				if (x < minStack.Peek())
+				{
+					minStack.Push(x);
+				}
+				else {
+					minStack.Push(minStack.Peek());
+				}
+			}
+		}
+
+		public void Pop()
+		{
+			stack.Pop();
+			minStack.Pop();
+		}
+
+		public int Top()
+		{
+			return stack.Peek();
+		}
+
+		public int GetMin()
+		{
+			return minStack.Peek();
+		}
+	}
+}
