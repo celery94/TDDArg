2bc1e00b6ffa0874e3b1a39d9c2970be25408aff
Celery Liu <liuct94@gmail.com>
2017-02-21 10:08:58 +0800

    400. Nth Digit not completed

---
+++ TDDArg.Test/NthDigitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDArg.Test
+{
+	[TestFixture]
+	public class NthDigitTest
+	{
+		[Test]
+		public void When_find_5_Then_return_5()
+		{
+			Assert.AreEqual(5, new NthDigit().FindNthDigit(5));
+		}
+
+		[Test]
+		public void When_find_11_Then_return_0()
+		{
+			Assert.AreEqual(0, new NthDigit().FindNthDigit(11));
+		}
+	}
+}
---
+++ TDDArg/NthDigit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TDDArg
+{
+	public class NthDigit
+	{
+		public int FindNthDigit(int n)
+		{
+			if (n <= 9)
+			{
+				return n;
+			}
+
+			return 0;
+		}
+	}
+}
