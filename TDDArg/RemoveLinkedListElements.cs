00a41aa2bf79593ebb1781a4ce6cd4adfe99f6a2
Celery Liu <liuct94@gmail.com>
2017-02-21 12:08:36 +0800

    203. Remove Linked List Elements passed

---
+++ TDDArg/RemoveLinkedListElements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace TDDArg
+{
+	public class RemoveLinkedListElements
+	{
+		public ListNode RemoveElements(ListNode head, int val)
+		{
+			while (head != null && head.val == val)
+			{
+				head = head.next;
+			}
+
+			if (head == null || head.next == null) return head;
+
+			var pre = head;
+			var current = head.next;
+
+			while (current != null)
+			{
+				var next = current.next;
+				if (current.val == val)
+				{
+					pre.next = next;
+					current = next;
+				}
+				else
+				{
+					pre = pre.next;
+					current = current.next;
+				}
+			}
+
+			return head;
+		}
+	}
+}
