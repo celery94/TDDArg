8dd70c98a24f6dd62d02751c28d06fb9e8e1ff20
Celery Liu <liuct94@gmail.com>
2017-02-21 12:38:36 +0800

    303. Range Sum Query - Immutable passed

---
+++ TDDArg/RangeSumQueryImmutable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace TDDArg
+{
+	public class NumArray
+	{
+		private int[] sums;
+
+		public NumArray(int[] nums)
+		{
+			sums = new int[nums.Length];
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if (i == 0)
+					sums[i] = nums[i];
+				else
+					sums[i] = sums[i - 1] + nums[i];
+			}
+		}
+
+		public int SumRange(int i, int j)
+		{
+			return i == 0 ? sums[j] : sums[j] - sums[i - 1];
+		}
+	}
+}
