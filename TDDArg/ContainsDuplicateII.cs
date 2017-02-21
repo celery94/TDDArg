55bc80dedc5536f5c2595d4885bd037187265317
Celery Liu <liuct94@gmail.com>
2017-02-21 12:17:47 +0800

    219. Contains Duplicate II with performance issue

---
+++ TDDArg/ContainsDuplicateII.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace TDDArg
+{
+	public class ContainsDuplicateII
+	{
+		public bool ContainsNearbyDuplicate(int[] nums, int k)
+		{
+			if (k == 0) return false;
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				var index = i + 1;
+				while (index <= i + k && index < nums.Length)
+				{
+					if (nums[i] == nums[index])
+					{
+						return true;
+					}
+					index++;
+				}
+			}
+
+			return false;
+		}
+	}
+}
