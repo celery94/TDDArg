c79bd0ffd97da905e405208b417a7c0c4a1a3b9a
Celery Liu <liuct94@gmail.com>
2017-02-21 09:15:01 +0800

    129. Sum Root to Leaf Numbers passed

---
+++ TDDArg/SumRootToLeafNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDDArg
+{
+	public class SumRootToLeafNumbers
+	{
+		public int SumNumbers(TreeNode root)
+		{
+			return Sum(root, 0);
+		}
+
+		private int Sum(TreeNode root, int s)
+		{
+			if (root == null) return 0;
+			if (root.left == null && root.right == null) return s * 10 + root.val;
+
+			return Sum(root.left, s * 10 + root.val) + Sum(root.right, s * 10 + root.val);
+		}
+	}
+}
