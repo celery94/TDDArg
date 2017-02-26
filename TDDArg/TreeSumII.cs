43fef9e9089029dffc1d12781f0012202e55be19
Celery Liu <liuct94@gmail.com>
2017-02-26 12:49:57 +0800

    three sum II failed

---
+++ TDDArg/TreeSumII.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TDDArg
+{
+	public class TreeSumII
+	{
+		public IList<IList<int>> ThreeSum(int[] nums)
+		{
+			List<IList<int>> result = new List<IList<int>>();
+
+			Find(result, new List<int>(), nums, 0);
+
+			return result;
+		}
+
+		private void Find(List<IList<int>> result, List<int> current, int[] nums, int index)
+		{
+			if (current.Sum() == 0 && current.Count == 3)
+			{
+				result.Add(new List<int>(current));
+				return;
+			}
+
+			for (int i = index; i < nums.Length; i++)
+			{
+				current.Add(nums[i]);
+				Find(result, current, nums, index + 1);
+				current.RemoveAt(current.Count - 1);
+			}
+		}
+	}
+}
