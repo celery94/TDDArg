96e302bd8af8414f0f0e944c3f1b0b0375e209f2
Celery Liu <liuct94@gmail.com>
2017-02-26 11:06:56 +0800

    KthLargestElementInAnArray

---
+++ TDDArg/KthLargestElementInAnArray.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TDDArg
+{
+	public class KthLargestElementInAnArray
+	{
+		public int FindKthLargest(int[] nums, int k)
+		{
+			Array.Sort(nums);
+			return nums[nums.Length - k];
+		}
+	}
+}
