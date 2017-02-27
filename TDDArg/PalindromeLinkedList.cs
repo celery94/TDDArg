1275d7c8191e50bd799e057592eec427d9326f9c
Celery Liu <liuct94@gmail.com>
2017-02-27 16:59:05 +0800

    234. Palindrome Linked List passed

---
+++ TDDArg/PalindromeLinkedList.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+
+namespace TDDArg
+{
+    [TestFixture]
+    public class PalindromeLinkedList
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head?.next == null) return true;
+
+            var slow = head;
+            var fast = head;
+
+            while (fast.next != null && fast.next.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            ListNode pre = null;
+            slow = slow.next;
+            var next = slow.next;
+
+            while (next != null)
+            {
+                slow.next = pre;
+                pre = slow;
+
+                slow = next;
+                next = slow.next;
+            }
+            slow.next = pre;
+
+
+            while (slow != null)
+            {
+                if (slow.val != head.val) return false;
+
+                slow = slow.next;
+                head = head.next;
+            }
+
+            return true;
+        }
+
+        [Test]
+        public void Given_list_When_check_Then_return()
+        {
+            var head = new ListNode(1)
+            {
+                next = new ListNode(1)
+                {
+                    next = new ListNode(2)
+                    {
+                        next = new ListNode(1)
+                    }
+                }
+            };
+
+            Assert.IsFalse(IsPalindrome(head));
+        }
+    }
+}
