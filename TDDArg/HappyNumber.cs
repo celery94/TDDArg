63dbf89eed45fde4cdec903ecc0b9b3b49db2abb
Celery Liu <liuct94@gmail.com>
2017-02-28 14:39:33 +0800

    202. Happy Number passed

---
+++ TDDArg/HappyNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TDDArg
+{
+    public class HappyNumber
+    {
+        public bool IsHappy(int n)
+        {
+            while (n > 6)
+            {
+                int next = 0;
+                while (n > 0)
+                {
+                    next += (n % 10) * (n % 10);
+                    n /= 10;
+                }
+                n = next;
+            }
+            return n == 1;
+        }
+    }
+}
