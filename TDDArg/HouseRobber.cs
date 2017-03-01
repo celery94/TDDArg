b1d00e6744126b55c1e72d16ef1aeb08df126aeb
Celery Liu <liuct94@gmail.com>
2017-03-01 13:10:14 +0800

    198. House Robber passed

---
+++ TDDArg/HouseRobber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TDDArg
+{
+    public class HouseRobber
+    {
+        public int Rob(int[] nums)
+        {
+            int rob = 0;
+            int notRob = 0;
+
+            foreach (var num in nums)
+            {
+                var currentRob = notRob + num;
+                notRob = Math.Max(rob, notRob);
+                rob = currentRob;
+            }
+
+            return Math.Max(rob, notRob);
+        }
+    }
+}
