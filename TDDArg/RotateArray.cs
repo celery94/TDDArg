d5de09bdf02b46e004655ed534cb2898eb8216da
Celery Liu <liuct94@gmail.com>
2017-03-01 10:29:24 +0800

    189. Rotate Array passed

---
+++ TDDArg/RotateArray.cs
@@ -0,0 +1,25 @@
+﻿namespace TDDArg
+{
+    public class RotateArray
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            k = k % nums.Length;
+            Reverse(nums, 0, nums.Length - k - 1);
+            Reverse(nums, nums.Length - k, nums.Length - 1);
+            Reverse(nums, 0, nums.Length - 1);
+        }
+
+        private void Reverse(int[] nums, int start, int end)
+        {
+            while (start < end)
+            {
+                var tmp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = tmp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
