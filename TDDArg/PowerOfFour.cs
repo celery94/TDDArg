928a095a4f49e2e5e6cf903a8736a8e42842ce44
Celery Liu <liuct94@gmail.com>
2017-03-06 19:11:03 +0800

    342. Power of Four passed

---
+++ TDDArg/PowerOfFour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace TDDArg
+{
+	public class PowerOfFour
+	{
+		public bool IsPowerOfFour(int num)
+		{
+			if (num == 0) return false;
+			if (num == 1) return true;
+
+			while (num % 4 == 0)
+			{
+				num = num / 4;
+				if (num == 1) return true;
+			}
+
+			return false;
+		}
+	}
+}
