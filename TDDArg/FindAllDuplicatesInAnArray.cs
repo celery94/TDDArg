f46b2f70594c8870e74bc27a89b9a430c901d51d
Celery Liu <liuct94@gmail.com>
2017-03-06 16:28:49 +0800

    442. Find All Duplicates in an Array passed

---
+++ TDDArg/FindAllDuplicatesInAnArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDDArg
+{
+    public class FindAllDuplicatesInAnArray
+    {
+        public IList<int> FindDuplicates(int[] nums)
+        {
+            List<int> result = new List<int>();
+            foreach (var num in nums)
+            {
+                var index = Math.Abs(num) - 1;
+                if (nums[index] < 0)
+                {
+                    result.Add(Math.Abs(num));
+                }
+                else
+                {
+                    nums[index] = 0 - nums[index];
+                }
+            }
+
+            return result;
+        }
+    }
+}
