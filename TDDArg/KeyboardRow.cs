2378322690c2904efe8a01dbcd51f6b9ecc7d77e
Celery Liu <liuct94@gmail.com>
2017-03-06 14:12:28 +0800

    500. Keyboard Row passed

---
+++ TDDArg/KeyboardRow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TDDArg
+{
+    public class KeyboardRow
+    {
+        public string[] FindWords(string[] words)
+        {
+            string[] keyboardRows = new string[] { "qwertyuiop", "asdfghjkl", "zxcvbnm" };
+
+            Dictionary<char, int> mapping = new Dictionary<char, int>();
+
+            for (int i = 0; i < keyboardRows.Length; i++)
+            {
+                foreach (var ch in keyboardRows[i])
+                {
+                    mapping.Add(ch, i);
+                }
+            }
+
+            return words.Where(word => word.ToLower().ToCharArray().Select(q => mapping[q]).Distinct().Count() == 1).ToArray();
+        }
+    }
+}
