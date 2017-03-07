12e6a89e93786fb838c377fc3e537a09000f1d59
Celery Liu <liuct94@gmail.com>
2017-03-07 09:41:25 +0800

    Insert Node in a Binary Search Tree

---
+++ TDDArg/InsertNodeInABinarySearchTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace TDDArg
+{
+	public class InsertNodeInABinarySearchTree
+	{
+		public TreeNode InsertNode(TreeNode root, TreeNode node)
+		{
+			if (root == null) return node;
+
+			if (root.val > node.val)
+			{
+				root.left = InsertNode(root.left, node);
+			}
+			else
+			{
+				root.right = InsertNode(root.right, node);
+			}
+
+			return root;
+		}
+
+		public TreeNode InsertNodeII(TreeNode root, TreeNode node)
+		{
+			if (root == null) return node;
+			if (node == null) return root;
+
+			var head = root;
+			while (root != null)
+			{
+				if (root.val >= node.val && root.left == null)
+				{
+					root.left = node;
+					break;
+				}
+				else if (root.val < node.val && root.right == null)
+				{
+					root.right = node;
+					break;
+				}
+				else if (root.val >= node.val)
+				{
+					root = root.left;
+				}
+				else
+				{
+					root = root.right;
+				}
+			}
+
+			return head;
+		}
+	}
+}
