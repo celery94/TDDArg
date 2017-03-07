0304b71e1c1c67d04a01203be89782b12f7b7817
Celery Liu <liuct94@gmail.com>
2017-03-07 10:14:11 +0800

    PathSumII

--- TDDArg/PathSumII.cs
+++ TDDArg/PathSumII.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace TDDArg
 {
+	[TestFixture]
 	public class PathSumII
 	{
 		public static IList<IList<int>> PathSum(TreeNode root, int sum)
@@ -23,7 +25,7 @@ namespace TDDArg
 			if (node.left == null && node.right == null && node.val == sum)
 			{
 				list.Add(currentPath);
-				currentPath.Remove(currentPath.Count - 1);
+				currentPath.RemoveAt(currentPath.Count - 1);
 				return;
 			}
 			else
@@ -32,7 +34,36 @@ namespace TDDArg
 				FindPathSum(node.right, sum - node.val, list, currentPath);
 			}
 
-			currentPath.Remove(currentPath.Count - 1);
+			currentPath.RemoveAt(currentPath.Count - 1);
+		}
+
+		[Test]
+		public void Test()
+		{
+			var tree = new TreeNode(5)
+			{
+				left = new TreeNode(4)
+				{
+					left = new TreeNode(11)
+					{
+						left = new TreeNode(7),
+						right = new TreeNode(2)
+					}
+				},
+				right = new TreeNode(8)
+				{
+					left = new TreeNode(13),
+					right = new TreeNode(4)
+					{
+						left = new TreeNode(5),
+						right = new TreeNode(1)
+					}
+				}
+			};
+
+			var result = PathSum(tree, 22);
+
+			Assert.AreEqual(2, result.Count);
 		}
 	}
 }
